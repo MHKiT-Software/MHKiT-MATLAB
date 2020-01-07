0677f84fcf056b4edef82139d781a2650fd11fef
Pauly <rpauly@nrel.gov>
2020-01-07 11:29:21 -0700

    forgot to remove files

--- mhkit/qc/qc_outlier.m
+++
@@ -1,5 +0,0 @@
-% Wrap QC functions around pecos monitoring check functions
-% This all assumes a pandas dataframe with a datetime index
-function results = qc_corrupt(data,vals)
-  results = struct(py.pecos.monitoring.check_corrupt(data,vals))
-end 
--- mhkit/qc/qc_timestep.m
+++
@@ -1,5 +0,0 @@
-% Wrap QC functions around pecos monitoring check functions
-% This all assumes a pandas dataframe with a datetime index
-function results = qc_timestep(data, freq)
-  results = struct(py.pecos.monitoring.check_timestamp(data,freq))
-end 
