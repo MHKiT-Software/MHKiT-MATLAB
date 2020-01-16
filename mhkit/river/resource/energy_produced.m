7a7b239df2370015c2ca502e19954e1d1a587e43
rpauly18 <rpauly@nrel.gov>
2020-01-16 10:35:54 -0700

    update to energy_produced

--- mhkit/river/resource/energy_produced.m
+++ mhkit/river/resource/energy_produced.m
@@ -1,4 +1,4 @@
-function E=energy_produced(F,P,seconds)
+function E=energy_produced(P,seconds)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %     Returns the energy produced for a given time period provided
@@ -6,21 +6,6 @@ function E=energy_produced(F,P,seconds)
 %     
 % Parameters
 % ----------
-%     F : Exceedance Probability [%]
-%
-%         Pandas dataframe indexed by time [datetime or s]:  
-%
-%           To make a pandas data frame from user supplied frequency and spectra
-%           use py.mhkit_python_utils.pandas_dataframe.timeseries_to_pandas(timeseries,time,x)
-%
-%         OR
-%
-%         structure of form:
-%
-%            F.F
-%
-%            F.time [s]
-%
 %     P : Power [W]
 %          
 %         Pandas dataframe indexed by time [datetime or s]:  
@@ -54,22 +39,6 @@ function E=energy_produced(F,P,seconds)
 py.importlib.import_module('mhkit_python_utils');
 py.importlib.import_module('mhkit');
 
-if (isa(F,'py.pandas.core.frame.DataFrame')~=1)
-    x=size(F.F);
-    li=py.list();
-    if x(2)>1 
-        for i = 1:x(2)
-            app=py.list(F.F(:,i));
-            li=py.mhkit_python_utils.pandas_dataframe.lis(li,app);
-            
-        end
-    elseif x(2) ==1 
-        li=F.F;
-    end
-
-
-    F=py.mhkit_python_utils.pandas_dataframe.timeseries_to_pandas(li,F.time,int32(x(2)));
-end
 
 if (isa(P,'py.pandas.core.frame.DataFrame')~=1)
     x=size(P.P);
@@ -88,4 +57,4 @@ if (isa(P,'py.pandas.core.frame.DataFrame')~=1)
     P=py.mhkit_python_utils.pandas_dataframe.timeseries_to_pandas(li,P.time,int32(x(2)));
 end
 
-E=py.mhkit.river.resource.energy_produced(F,P,seconds);
+E=py.mhkit.river.resource.energy_produced(P,seconds);
