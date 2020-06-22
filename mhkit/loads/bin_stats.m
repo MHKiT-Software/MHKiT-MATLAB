a8e5fc68e6794043ebeb5d404fb99c8caafff35d
rpauly18 <rpauly@nrel.gov>
2020-06-22 11:00:22 -0600

    got bin_stats working

--- mhkit/loads/bin_stats.m
+++ mhkit/loads/bin_stats.m
@@ -30,28 +30,48 @@ py.importlib.import_module('mhkit');
 py.importlib.import_module('numpy');
 
 
-if (isa(data,'py.pandas.core.frame.DataFrame')~=1)
-    if (isstruct(data)==1)
-        dsize=size(data.data);
-        li=py.list();
-        if dsize(2)>1 
-            for i = 1:dsize(2)
-                app=py.list(data.data(:,i));
-                li=py.mhkit_python_utils.pandas_dataframe.lis(li,app);
-            
-            end
-            datapd=py.mhkit_python_utils.pandas_dataframe.spectra_to_pandas(data.time(:,1),li,int32(dsize(2)));
-        elseif x(2)==1
-            datapd=py.mhkit_python_utils.pandas_dataframe.spectra_to_pandas(data.time,py.numpy.array(data.data),int32(dsize(2)));
-        end
+fn = fieldnames(data);
+si = size(fn);
+li=py.list();
+li2 = py.list();
+ 
+
+ for k=1:length(fn)
+     
+     if ~strcmp(fn{k} , 'time')
+
+            eval(['temp = data.' fn{k} ';' ]); 
+            indlen = size(temp);
+            app=py.list(temp);
+            li=py.mhkit_python_utils.pandas_dataframe.lis(li,app);
+            li2=py.mhkit_python_utils.pandas_dataframe.lis(li2,fn{k});
         
-    else
-        ME = MException('MATLAB:bin_stats','data needs to be a structure or Pandas dataframe, use py.mhkit_python_utils.pandas_dataframe.spectra_to_pandas to create one');
-        throw(ME);
-    end
+     end
+ end
+ ind = 1:1:indlen(1);
+datapd=py.mhkit_python_utils.pandas_dataframe.spectra_to_pandas(ind,li,si(1),pyargs('cols',li2));
+
+stat_py = py.mhkit.loads.bin_stats(datapd,py.numpy.array(x),py.numpy.array(bin_edges));
+
+averages = double(py.array.array('d',py.numpy.nditer(stat_py{1})));
+std = double(py.array.array('d',py.numpy.nditer(stat_py{2})));
+sha=cell(stat_py{1}.values.shape);
+x=int64(sha{1,1});
+y=int64(sha{1,2});
+pointer = 1;
+
+for k=1:length(fn)
+     if ~strcmp(fn{k} , 'time')
+        val = averages(pointer:pointer+x-1);
+        eval(['datast.averages.' fn{k} '= val ;' ]);
+        pointer = pointer +x ;
+     end
 end
-
-averages,bstd = py.mhkit.loads.bin_stats(datapd,numpy.array(x),numpy.array(bin_edges));
-
-datast.averages = double(py.array.array('d',py.numpy.nditer(averages)));
-datast.std = bstd;
+ pointer = 1;
+ for k=1:length(fn)
+     if ~strcmp(fn{k} , 'time')
+        val = std(pointer:pointer+x-1);
+        eval(['datast.std.' fn{k} '= val ;' ]);
+        pointer = pointer +x; 
+     end
+ end
