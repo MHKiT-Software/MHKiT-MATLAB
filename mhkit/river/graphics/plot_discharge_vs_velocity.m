84777966fbc8f717919c9c19ce06b25b4cc4400f
rpauly18 <rpauly@nrel.gov>
2020-07-27 12:40:02 -0600

    removing unnecessary code

--- mhkit/river/graphics/plot_discharge_vs_velocity.m
+++ mhkit/river/graphics/plot_discharge_vs_velocity.m
@@ -42,45 +42,6 @@ grid on
 xlabel('Discharge [m^{3}/s]','FontSize',20)
 ylabel('Velocity [m/s]','FontSize',20)
 
-% if nargin == 3
-%     if isstring(varargin{1})
-%         title(varargin{1})
-%     elseif isnumeric(varargin{1})
-%         mi=min(Q);
-%         
-%         ma=max(Q);
-%         si=size(varargin{1});
-%         x=linspace(mi(1),ma(1),100);
-%         hold on
-%         p2=plot(x,polyval(varargin{1},x),'--');
-%         legend(p2,{'Polynomial Fit'},'Location','northwest')
-%     else
-%         ME = MException('MATLAB:plot_discharge_vs_velocity','Variable argument is of the wrong type');
-%         throw(ME);
-%     end
-%     
-% elseif nargin ==4 
-%     mi=min(Q);
-%     ma=max(Q);
-%     if isstring(varargin{1}) & isnumeric(varargin{2})
-%         
-%         si=size(varargin{2});
-%         x=linspace(mi(1),ma(1),100);
-%         hold on
-%         p2=plot(x,polyval(varargin{2},x),'--');
-%         legend(p2,{'Polynomial Fit'},'Location','northwest')
-%         title(varargin{1})
-%     elseif isstring(varargin{2}) & isnumeric(varargin{1})
-%         si=size(varargin{1});
-%         x=linspace(mi(1),ma(1),100);
-%         hold on
-%         p2=plot(x,polyval(varargin{1},x),'--');
-%         legend(p2,{'Polynomial Fit'},'Location','northwest')
-%         title(varargin{2})
-%     else
-%         ME = MException('MATLAB:plot_discharge_vs_velocity','Variable argument is of the wrong type');
-%         throw(ME);
-%     end
 title(options.title)
 mi=min(Q);
 ma=max(Q);
--- mhkit/river/graphics/plot_velocity_vs_power.m
+++ mhkit/river/graphics/plot_velocity_vs_power.m
@@ -42,49 +42,6 @@ grid on
 xlabel('Velocity m/s]','FontSize',20)
 ylabel('Power [W]','FontSize',20)
 
-% if nargin == 3
-%     if isstring(varargin{1})
-%         title(varargin{1})
-%     elseif isnumeric(varargin{1})
-%         mi=min(V);
-%         
-%         ma=max(V);
-%         si=size(varargin{1});
-%         x=linspace(mi(1),ma(1),100);
-%         hold on
-%         p2=plot(x,polyval(varargin{1},x),'--');
-%         legend(p2,{'Polynomial Fit'},'Location','northwest')
-%     else
-%         ME = MException('MATLAB:plot_velocity_vs_power','Variable argument is of the wrong type');
-%         throw(ME);
-%     end
-%     
-% elseif nargin ==4 
-%     mi=min(V);
-%     ma=max(V);
-%     if isstring(varargin{1}) & isnumeric(varargin{2})
-%         si=size(varargin{2});
-%         x=linspace(mi(1),ma(1),100);
-%         hold on
-%         p2=plot(x,polyval(varargin{2},x),'--');
-%         legend(p2,{'Polynomial Fit'},'Location','northwest')
-%         title(varargin{1})
-%     elseif isstring(varargin{2}) & isnumeric(varargin{1})
-%         si=size(varargin{1});
-%         x=linspace(min(V),max(V),100);
-%         hold on
-%         p2=plot(x,polyval(varargin{1},x),'--');
-%         legend(p2,{'Polynomial Fit'},'Location','northwest')
-%         title(varargin{2})
-%     else
-%         ME = MException('MATLAB:plot_velocity_vs_power','Variable argument is of the wrong type');
-%         throw(ME);
-%     end
-% elseif nargin > 4
-%     ME = MException('MATLAB:plot_velocity_vs_power','Too many arguments given');
-%         throw(ME);
-% end
-
 title(options.title)
 mi=min(V);
 ma=max(V);
