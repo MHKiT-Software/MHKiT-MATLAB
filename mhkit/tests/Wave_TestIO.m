998b2e7259f29c3816b7adc415820ca4a300ad17
rpauly18 <rpauly@nrel.gov>
2020-10-01 13:53:46 -0600

    adding new IO tests

--- mhkit/tests/Wave_TestIO.m
+++ mhkit/tests/Wave_TestIO.m
@@ -41,5 +41,38 @@ classdef Wave_TestIO < matlab.unittest.TestCase
             data = read_NDBC_file("../../examples/data/wave/data.txt");
             assertEqual(testCase,size(data.spectra),[743 47]);
         end
+        
+        function test_ndbc_available_data(testCase)
+            data = NDBC_available_data('swden', 'buoy_number','46029');
+            columns = data.Properties.VariableNames;
+            columns_expected = [{'Station_id'},{'year'},{'file'}];
+            assertEqual(testCase,columns,columns_expected);
+            number_years = length(data.year);
+            expected_years = [1996:1996+(number_years-1)]';
+            assertEqual(testCase,str2num(char(data.year)),expected_years);
+            
+            h = height(data);
+            w = width(data);
+            assertEqual(testCase,[h,w],[number_years,3]);
+            
+         
+        end
+        
+        function test_ndbc_request_data(testCase)
+            filenames=["46042w1996.txt.gz";... 
+                        "46029w1997.txt.gz";.... 
+                        "46029w1998.txt.gz"];
+            ndbc_data = NDBC_request_data('swden', filenames);
+            ndbc_data = struct2table(ndbc_data.ID_46042.year_1996,'AsArray',true);
+            file = gunzip('../../examples/data/wave/46042w1996.txt.gz');
+            expected_data = readmatrix(file{1});
+            
+            data_array = table2array(ndbc_data(:,[1,2,3,4,6]));
+            cat_data = cat(2,data_array{:});
+            
+            assertEqual(testCase,cat_data,expected_data(2:end,:));
+            
+         
+        end
     end
 end
