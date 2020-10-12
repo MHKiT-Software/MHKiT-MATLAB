d8739904bcdc10e462857cd59b4e1c7f7795b1c8
rpauly18 <rpauly@nrel.gov>
2020-10-12 13:50:40 -0600

    removing calls to statistics toolbox

--- mhkit/tests/Wave_TestPerformance.m
+++ mhkit/tests/Wave_TestPerformance.m
@@ -3,52 +3,88 @@ classdef Wave_TestPerformance < matlab.unittest.TestCase
     methods (Test)
 
         function test_capture_length(testCase)
-            Obj.P = normrnd(200, 40, [1,100000]);
-            Obj.J = normrnd(300, 10, [1,100000]);
+            a = 40;
+            b = 200;
+            Obj.P = (b-a).*rand(1,100000) + a;
+            %Obj.P = normrnd(200, 40, [1,100000]);
+            a = 10;
+            b = 300;
+            Obj.J = (b-a).*rand(1,100000) + a;
+            %Obj.J = normrnd(300, 10, [1,100000]);
 
             L = capture_length(Obj.P, Obj.J);
-%             L_stats = statistics(L)
-%             assertAlmostEqual(testCase,L_stats('mean'), 0.6676, 3);
+            L_stats = mean(L);
+            assertEqual(testCase,L_stats, 1.4, 'RelTol',0.1);
         end
 
         function test_capture_length_matrix(testCase)
             seednum = 123;
             rng(seednum);
-            Obj.Te = normrnd(4.5, 0.8, [1,100000]);
-            Obj.P = normrnd(200, 40, [1,100000]);
-            Obj.J = normrnd(300, 10, [1,100000]);
-            Obj.Hm0 = raylrnd(4, [1,100000]);
+            a = 0.8;
+            b = 4.5;
+            Obj.Te = (b-a).*randn(1,100000) + a;
+            %Obj.Te = normrnd(4.5, 0.8, [1,100000]);
+            a = 40;
+            b = 200;
+            Obj.P = (b-a).*randn(1,100000) + a;
+            %Obj.P = normrnd(200, 40, [1,100000]);
+            a = 10;
+            b = 300;
+            Obj.J = (b-a).*randn(1,100000) + a;
+            %Obj.J = normrnd(300, 10, [1,100000]);
+            sigma = 4;
+            Obj.Hm0 = abs(sigma*randn(1,100000)+1i*sigma*randn(1,100000));
+            %Obj.Hm0 = raylrnd(4, [1,100000]);
             Obj.Hm0_bins = 0:0.5:18.5;
             Obj.Te_bins = 0:1:8;
             
             L = capture_length(Obj.P, Obj.J);
             LM = capture_length_matrix(Obj.Hm0, Obj.Te, L, 'std', Obj.Hm0_bins, Obj.Te_bins);
+            
             assertEqual(testCase,size(LM.values), [38 9]);
-            assertEqual(testCase,sum(sum(isnan(LM.values))), 131);
+            assertEqual(testCase,sum(sum(isnan(LM.values))), 34);
         end
 
         function test_wave_energy_flux_matrix(testCase)
             seednum = 123;
             rng(seednum);
-            Obj.Te = normrnd(4.5, 0.8, [1,100000]);
-            Obj.P = normrnd(200, 40, [1,100000]);
-            Obj.J = normrnd(300, 10, [1,100000]);
-            Obj.Hm0 = raylrnd(4, [1,100000]);
+            a = 0.8;
+            b = 4.5;
+            Obj.Te = (b-a).*randn(1,100000) + a;
+            %Obj.Te = normrnd(4.5, 0.8, [1,100000]);
+            a = 40;
+            b = 200;
+            Obj.P = (b-a).*randn(1,100000) + a;
+            %Obj.P = normrnd(200, 40, [1,100000]);
+            a = 10;
+            b = 300;
+            Obj.J = (b-a).*randn(1,100000) + a;
+            sigma = 4;
+            Obj.Hm0 = abs(sigma*randn(1,100000)+1i*sigma*randn(1,100000));
             Obj.Hm0_bins = 0:0.5:18.5;
             Obj.Te_bins = 0:1:8;
             
             JM = wave_energy_flux_matrix(Obj.Hm0, Obj.Te,Obj.J, 'mean', Obj.Hm0_bins, Obj.Te_bins);
             assertEqual(testCase,size(JM.values), [38 9]);
-            assertEqual(testCase,sum(sum(isnan(JM.values))), 131);
+            assertEqual(testCase,sum(sum(isnan(JM.values))), 34);
         end
 
         function test_power_matrix(testCase)
             seednum = 123;
             rng(seednum);
-            Obj.Te = normrnd(4.5, 0.8, [1,100000]);
-            Obj.P = normrnd(200, 40, [1,100000]);
-            Obj.J = normrnd(300, 10, [1,100000]);
-            Obj.Hm0 = raylrnd(4, [1,100000]);
+            a = 0.8;
+            b = 4.5;
+            Obj.Te = (b-a).*randn(1,100000) + a;
+            %Obj.Te = normrnd(4.5, 0.8, [1,100000]);
+            a = 40;
+            b = 200;
+            Obj.P = (b-a).*randn(1,100000) + a;
+            %Obj.P = normrnd(200, 40, [1,100000]);
+            a = 10;
+            b = 300;
+            Obj.J = (b-a).*randn(1,100000) + a;
+            sigma = 4;
+            Obj.Hm0 = abs(sigma*randn(1,100000)+1i*sigma*randn(1,100000));
             Obj.Hm0_bins = 0:0.5:18.5;
             Obj.Te_bins = 0:1:8;
             
@@ -57,15 +93,21 @@ classdef Wave_TestPerformance < matlab.unittest.TestCase
             JM = wave_energy_flux_matrix(Obj.Hm0, Obj.Te,Obj.J, 'mean', Obj.Hm0_bins, Obj.Te_bins);
             PM = power_matrix(LM, JM);
             assertEqual(testCase,size(PM.values), [38 9]);
-            assertEqual(testCase,sum(sum(isnan(PM.values))), 131);
+            assertEqual(testCase,sum(sum(isnan(PM.values))), 34);
         end
 
         function test_mean_annual_energy_production(testCase)
             import matlab.unittest.constraints.IsEqualTo
             import matlab.unittest.TestCase
             import matlab.unittest.constraints.AbsoluteTolerance
-            Obj.P = normrnd(200, 40, [1,100000]);
-            Obj.J = normrnd(300, 10, [1,100000]);
+            
+            a = 40;
+            b = 200;
+            Obj.P = (b-a).*randn(1,100000) + a;
+            %Obj.P = normrnd(200, 40, [1,100000]);
+            a = 10;
+            b = 300;
+            Obj.J = (b-a).*randn(1,100000) + a;
             
             L = capture_length(Obj.P, Obj.J);
             maep = mean_annual_energy_production_timeseries(L, Obj.J);
@@ -80,10 +122,19 @@ classdef Wave_TestPerformance < matlab.unittest.TestCase
             
             seednum = 123;
             rng(seednum);
-            Obj.Te = normrnd(4.5, 0.8, [1,100000]);
-            Obj.P = normrnd(200, 40, [1,100000]);
-            Obj.J = normrnd(300, 10, [1,100000]);
-            Obj.Hm0 = raylrnd(4, [1,100000]);
+            a = 0.8;
+            b = 4.5;
+            Obj.Te = (b-a).*randn(1,100000) + a;
+            %Obj.Te = normrnd(4.5, 0.8, [1,100000]);
+            a = 40;
+            b = 200;
+            Obj.P = (b-a).*randn(1,100000) + a;
+            %Obj.P = normrnd(200, 40, [1,100000]);
+            a = 10;
+            b = 300;
+            Obj.J = (b-a).*randn(1,100000) + a;
+            sigma = 4;
+            Obj.Hm0 = abs(sigma*randn(1,100000)+1i*sigma*randn(1,100000));
             Obj.Hm0_bins = 0:0.5:18.5;
             Obj.Te_bins = 0:1:8;
             M = wave_energy_flux_matrix(Obj.Hm0,Obj.Te,Obj.J, 'mean', Obj.Hm0_bins, Obj.Te_bins);
