2d27a3dcc785c5f94ea69f4bbd0644cc778116ca
rpauly18 <rpauly@nrel.gov>
2021-01-04 09:10:29 -0700

    modifying autoupdate and moving legend location

--- mhkit/wave/graphics/plot_chakrabarti.m
+++ mhkit/wave/graphics/plot_chakrabarti.m
@@ -9,10 +9,10 @@ function figure=plot_chakrabarti(H, lambda_w, D, options)
 % 
 % Parameters
 % ------------
-%   H: integer, double or vector
+%    H: integer, double or vector
 %         Wave height [m]
 %
-%   lambda_w: integer, double or vector
+%    lambda_w: integer, double or vector
 %         Wave length [m]
 %
 %    D: integer, double or vector of 
@@ -24,9 +24,9 @@ function figure=plot_chakrabarti(H, lambda_w, D, options)
 %         
 % Returns
 % ---------
-%	figure: figure
-%       Plots wave force regime as Keulegan-Carpenter parameter versus
-%       diffraction parameter
+%	 figure: figure
+%         Plots wave force regime as Keulegan-Carpenter parameter versus
+%         diffraction parameter
 %
 % Examples
 %     --------
@@ -50,6 +50,16 @@ arguments
     options.savepath = "";
 end
 
+if any([~isnumeric(H), ~isnumeric(lambda_w), ~isnumeric(D)])
+    ME = MException('MATLAB:plot_chakrabarti','H,lambda_w, and D must be numbers');
+    throw(ME);
+end
+
+if any([length(H) ~= length(lambda_w), length(lambda_w) ~= length(D)])
+    ME = MException('MATLAB:plot_chakrabarti','H,lambda_w, and D must all be the same length');
+    throw(ME);
+end
+
 szD = size(D);
 KC = zeros(szD);
 Diffraction = zeros(szD);
@@ -58,7 +68,7 @@ for i=1:max(szD)
     Diffraction(i) = pi.*D(i) ./ lambda_w(i);
     txt = ('H='+string(H(i))+', {\lambda_w}='+string(lambda_w(i))+', D='+string(D(i)));
     figure = loglog(Diffraction(i), KC(i),'o', 'DisplayName', txt);
-    legend('Location', 'northeast', 'NumColumns', 2);
+    legend('Location', 'southoutside', 'NumColumns', 2);
     xlabel('Diffraction parameter, $\frac{\pi*D}{\lambda_w}$','Interpreter','latex')
     ylabel('KC parameter, $\frac{H}{D}$','Interpreter','latex')
     set(figure, 'markerfacecolor', get(figure, 'color'))
@@ -67,103 +77,74 @@ end
 
 if any(KC>=10) || any(KC<=.02) || any(Diffraction>=50) || any(lambda_w >= 1000)
     axis auto
+
 else
     xlim([0.01 10])
     ylim([0.01 50])
+    
 end
-
-%check to see if the characteristic length D is a number
-if any(~isnumeric(D))
-    ME = MException('MATLAB:plot_chakrabarti','D must be a number');
-    throw(ME);
-end
-
-%check to see if the wave height H is a number
-if any(~isnumeric(H))
-    ME = MException('MATLAB:plot_chakrabarti','H must be a number');
-    throw(ME);
-end
-
-%check to see if the wave length lambda_w is a number
-if any(~isnumeric(lambda_w))
-    ME = MException('MATLAB:plot_chakrabarti','lambda_w must be a number');
-    throw(ME);
-end
-
-%check to see if D, H, and lambda_w are the same size
-if any(~isequal(size(D),size(H),size(lambda_w)))
-    ME = MException('MATLAB:plot_chakrabarti','D, H, and lambda_w must be the same size');
-    throw(ME);
-end
-
+xDrag = 0.0125;
+xInertiaDrag = 0.02;
+yDrag = 25;
+yInertiaDrag = 8;
+yWaveBreaking1 = 2; 
+yWaveBreaking2 = 1.25;
+x1 = xlim;
+graphScale_x = x1(1);
 legend show
 legend('AutoUpdate','off')
 
-x1 = xlim;
-if x1(1) >= 0.01
-    graphScale_x = 0.01;
-    xDrag = 0.0125;
-    xInertiaDrag = 0.02;
-    yDrag = 25;
-    yInertiaDrag = 8;
-%     xWaveBreaking1 = 
-    yWaveBreaking1 = 2;
-%     xWaveBreaking2 = 
-    yWaveBreaking2 = 1.25;
-else
-    graphScale_x = x1(1);
-    xDrag = 0.003;
-    xInertiaDrag = 0.008;
-    yDrag = 40;
-    yInertiaDrag = 8;
-%     xWaveBreaking1 = 
-    yWaveBreaking1 = 8;
-%     xWaveBreaking2 = 
-    yWaveBreaking2 = 4.5;
-end
+
+ if x1(1) >= 0.01
+     graphScale_x = 0.01;
+       
+ end
 
 hold on
 % deep water breaking limit (H/lambda_w = 0.14)
-x1 = logspace(1,log10(graphScale_x),2);
-y_breaking = 0.14*pi./x1;
-plot(x1, y_breaking, 'k')
+x2 = logspace(1,log10(graphScale_x),2);
+y_breaking = 0.14*pi./x2;
+plot(x2, y_breaking, 'k')
 text(1,yWaveBreaking1,'{\it Wave Breaking}','HorizontalAlignment','center','FontSize',8)
 text(1,yWaveBreaking2,'$\frac{H}{\lambda_w} > 0.14$','Interpreter','latex','HorizontalAlignment','center','FontSize',8)
 
 hold on
+gs_x = min(x2);
+gs2 = max(x2);
 % upper bound of low drag region
     ldv = 20;
     ldh = 0.14*pi/ldv;
-    line([graphScale_x,ldh],[ldv,ldv], 'Color','k','LineStyle','--')
-%     x2 = 0.14*pi/ldv;
-%     y_small_drag = 20;
-%     plot(x2, y_small_drag,'--k')
+    line([gs_x,ldh],[ldv,ldv], 'Color','k','LineStyle','--')
     text(xDrag,yDrag,'{\it Drag}','HorizontalAlignment','center','FontSize',8)
 
 hold on
 % upper bound of small drag region
     sdv = 1.5;
     sdh = 0.14*pi/sdv;
-    line([graphScale_x,sdh],[sdv,sdv], 'Color','k','LineStyle','--')
-    text(xInertiaDrag,yInertiaDrag,'{\it Inertia & Drag}','HorizontalAlignment','center','FontSize',8)
+    line([gs_x,sdh],[sdv,sdv], 'Color','k','LineStyle','--')
+    text(gs_x*2.5,yInertiaDrag,'{\it Inertia & Drag}','HorizontalAlignment','center','FontSize',8)
 
 hold on        
 % upper bound of negligible drag region
     ndv = 0.25;
     ndh = 0.14*pi/ndv;
-    line([graphScale_x,ndh],[ndv,ndv], 'Color','k','LineStyle','--')
-    text(8*graphScale_x,0.7,'{\it Large Inertia}','HorizontalAlignment','center','FontSize',8)
+    line([gs_x,ndh],[ndv,ndv], 'Color','k','LineStyle','--')
+    text(8*gs_x,0.7,'{\it Large Inertia}','HorizontalAlignment','center','FontSize',8)
 
-    text(8*graphScale_x,6e-2,'{\it All Inertia}','HorizontalAlignment','center','FontSize',8)
+    text(8*gs_x,8e-2,'{\it All Inertia}','HorizontalAlignment','center','FontSize',8)
     
 hold on
 % left bound of diffraction region
     drh = 0.5;
     drv = 0.14*pi/drh;
-    line([drh,drh],[graphScale_x,drv], 'Color','k','LineStyle','--')
-    text(2,6e-2,'{\it Diffraction}','HorizontalAlignment','center','FontSize',8)
+    line([drh,drh],[gs_x,drv], 'Color','k','LineStyle','--')
+    text(2,8e-2,'{\it Diffraction}','HorizontalAlignment','center','FontSize',8)
+
+axis([gs_x gs2 min(y_breaking) max(y_breaking)])
 
 len = strlength(options.savepath);
 if len > 1
     saveas(figure, options.savepath);
 end 
+
+hold off
