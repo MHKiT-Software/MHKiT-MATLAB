026dcd4dfcfa4d1f6df6107cfdd549e4afb868bb
Matthew Boyd <mr.matthew.boyd@gmail.com>
2021-10-29 14:13:15 -0600

    Setup CI, v17.1, uninstall all pre-installed toolboxes

---
+++ mhkit/utils/uninstall_all_toolboxes.m
@@ -0,0 +1,39 @@
+function result=uninstall_all_toolboxes()
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%     Uninstall all currently installed toolboxes
+%     
+% Parameters
+% ------------
+%     none 
+%
+% Returns
+% ---------
+%     result: boolean
+%         0 = success, 1 = failure, not all toolboxes uninstalled
+%
+%         
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+toolboxes = matlab.addons.toolbox.installedToolboxes;
+
+if height(toolboxes) < 1
+    result = 0;
+else
+    % Note: this cannot be vectorized
+    result = 0;
+    for i = 1:height(toolboxes)
+        try
+            output = matlab.addons.toolbox.uninstallToolbox(toolboxes(i));
+        catch
+            result = 1;
+            return;
+        end
+
+        if isempty(output)
+            result = 0 | result;
+        else
+            result = 1;
+        end
+    end
+end
