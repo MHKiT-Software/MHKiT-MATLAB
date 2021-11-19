5f88f7f1d2eadc14ec777e8cd9328b69b1b43eae
Matthew Boyd <mr.matthew.boyd@gmail.com>
2021-11-19 15:07:11 -0700

    Setup CI, v17.13, start uninstalling toolboxes

--- mhkit/utils/uninstall_all_toolboxes.m
+++ mhkit/utils/uninstall_all_toolboxes.m
@@ -15,22 +15,31 @@ function result=uninstall_all_toolboxes()
 %         
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% toolboxes = matlab.addons.toolbox.installedToolboxes;
-addons = matlab.addons.installedAddons;
+%%
+installedToolbox = struct;
+installedToolbox.Name = "Mixed-Signal Blockset";
+installedToolbox.Version = "2.1";
+installedToolbox.Guid = "AM";
+matlab.addons.toolbox.uninstallToolbox(installedToolbox);
+result = 0;
+%%
 
-if height(addons) < 1
-    result = 0;
-else
-    % Note: this cannot be vectorized
-    result = 0;
-    for i = 1:height(addons)
-        % output = matlab.addons.toolbox.uninstallToolbox(toolboxes(i));
-        if strcmpi('matlab', addons{i,1})
-            continue;
-        else
-            matlab.addons.disableAddon(addons{i,1});
-            result = matlab.addons.isAddonEnabled(addons{i,1});
-            result = 0 | result;
-        end
-    end
-end
+% toolboxes = matlab.addons.toolbox.installedToolboxes;
+% addons = matlab.addons.installedAddons;
+% 
+% if height(addons) < 1
+%     result = 0;
+% else
+%     % Note: this cannot be vectorized
+%     result = 0;
+%     for i = 1:height(addons)
+%         % output = matlab.addons.toolbox.uninstallToolbox(toolboxes(i));
+%         if strcmpi('matlab', addons{i,1})
+%             continue;
+%         else
+%             matlab.addons.disableAddon(addons{i,1});
+%             result = matlab.addons.isAddonEnabled(addons{i,1});
+%             result = 0 | result;
+%         end
+%     end
+% end
