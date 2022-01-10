0cf0143a18c9e2a5e104c86980256cc331027f8e
hivanov-nrel <53838851+hivanov-nrel@users.noreply.github.com>
2022-01-10 14:38:38 -0700

    WPTO for Matlab (#61)
    * wpto playground
    
    * format changes
    
    * upload temporary test script
    
    * wpto extract with REST API
    
    * fix directional wave spectrum bug
    
    * upload hindcast example
    
    * auto format time_index into datetime array
    
    * add tests for hindcast
    
    * remove temp file
    
    * bug fix for available buoy data vs hindcast
    
    * upload data
    
    * moving test files into hindcast folder
    
    * updating toolbox paths
    
    * updating hindcast example
    
    * Setup CI, v16.2, try to fix HDF5 issue, try fix outlined in Troubleshooting
    
    * Setup CI, v16.3, try to fix HDF5 issue, try fix outlined in Troubleshooting
    
    * Setup CI, v16.4, try to fix HDF5 issue, try fix outlined in Troubleshooting
    
    * Setup CI, v16.5, try to fix HDF5 issue, try fix outlined in Troubleshooting
    
    * Setup CI, v16.6, try to fix HDF5 issue, try fix outlined in Troubleshooting
    
    * Setup CI, v16.7, list installed toolboxes and addons
    
    * Setup CI, v17, uninstall all pre-installed toolboxes
    
    * Setup CI, v17.1, uninstall all pre-installed toolboxes
    
    * Setup CI, v17.2, toggle off all pre-installed toolboxes
    
    * Setup CI, v17.3, toggle off all pre-installed toolboxes
    
    * Setup CI, v17.4, toggle off all pre-installed toolboxes
    
    * Setup CI, v17.5, toggle off all pre-installed toolboxes
    
    * Setup CI, v17.6, toggle off all pre-installed toolboxes
    
    * Setup CI, v17.7, toggle off all pre-installed toolboxes
    
    * Setup CI, v17.8, toggle off all pre-installed toolboxes
    
    * Setup CI, v17.9, toggle off all pre-installed toolboxes
    
    * Setup CI, v17.10, list installed addons and toolboxes
    
    * Setup CI, v17.11, start uninstalling toolboxes
    
    * Setup CI, v17.12, start uninstalling toolboxes
    
    * Setup CI, v17.13, start uninstalling toolboxes
    
    * Setup CI, v17.14, start uninstalling toolboxes
    
    * Setup CI, v17.15, start uninstalling toolboxes
    
    * Setup CI, v17.16, start uninstalling toolboxes
    
    * Setup CI, v17.17, start uninstalling toolboxes
    
    * Setup CI, v17.18, start uninstalling toolboxes
    
    * Setup CI, v17.19, start uninstalling toolboxes
    
    * Setup CI, v17.20, start uninstalling toolboxes
    
    * Setup CI, v17.21, start uninstalling toolboxes
    
    * Setup CI, v17.22, start uninstalling toolboxes
    
    * Setup CI, v17.23, start uninstalling toolboxes
    
    * Setup CI, v17.24, start uninstalling toolboxes
    
    * Setup CI, v17.25, start uninstalling toolboxes
    
    * Setup CI, v17.26, start uninstalling toolboxes
    
    * Setup CI, v17.27, start uninstalling toolboxes
    
    * Setup CI, v17.28, start uninstalling toolboxes
    
    * Setup CI, v17.29, start uninstalling toolboxes
    
    * Setup CI, v17.30, start uninstalling toolboxes
    
    * Setup CI, v17.31, start uninstalling toolboxes
    
    * Setup CI, v17.32, start uninstalling toolboxes
    
    * Setup CI, v17.33, start uninstalling toolboxes
    
    * Setup CI, v17.34, start uninstalling toolboxes
    
    * Setup CI, v17.35, start uninstalling toolboxes
    
    * Setup CI, v18, simplify to most basic test
    
    * Setup CI, v19, install mhkit toolbox
    
    * Setup CI, v20, run a single test that uses mhkit
    
    * Setup CI, v21, run a single different test that uses mhkit
    
    * Setup CI, v21.1, run a single different test that uses mhkit
    
    * Setup CI, v21.2, run a single different test that uses mhkit
    
    * Add function to reload python in matlab after python code changes
    
    * Fix paths to python usgs and noaa functions
    
    * Update pierson_moskowitz_spectrum wrapper with additional Hs parameter
    
    * Update create_spectra with additional Hs parameter
    
    * Update tests fixing mostly broken relative file paths
    
    Co-authored-by: rpauly18 <rpauly@nrel.gov>
    Co-authored-by: Matthew Boyd <mr.matthew.boyd@gmail.com>

--- mhkit/tests/Wave_TestIO.m
+++ mhkit/tests/Wave_TestIO.m
@@ -7,7 +7,9 @@ classdef Wave_TestIO < matlab.unittest.TestCase
             Obj.expected_columns_metRT = struct('WDIR',{},'units',{},'WSPD',{},'GST',{},'WVHT',{},'DPD',{},'APD',{},'MWD',{},'PRES',{},'ATMP',{},'WTMP',{},'DEWP',{},'VIS',{},'PTDY',{},'TIDE',{},'time',{});
             Obj.expected_units_metRT = struct('WDIR',{"degT"},'WSPD',{"m/s"},'GST',{"m/s"},'WVHT',{"m"},'DPD',{"sec"},'APD',{"sec"},'MWD',{"degT"},'PRES',{"hPa"},'ATMP',{"degC"},'WTMP',{"degC"},'DEWP',{"degC"},'VIS',{"nmi"},'PTDY',{"hPa"},'TIDE',{"ft"});
 
-            data = read_NDBC_file("../../examples/data/wave/46097.txt");
+            relative_file_name = "../../examples/data/wave/46097.txt";
+            full_file_name = fullfile(fileparts(mfilename('fullpath')), relative_file_name);
+            data = read_NDBC_file(full_file_name);
             datarm1 = rmfield(data,'units');
             datarm2 = rmfield(datarm1,'time');
             expected_index0 = datestr(datenum(2019,4,2,13,50,0));
@@ -24,7 +26,9 @@ classdef Wave_TestIO < matlab.unittest.TestCase
             Obj.expected_columns_metH = struct('WDIR',{},'units',{},'WSPD',{},'GST',{},'WVHT',{},'DPD',{},'APD',{},'MWD',{},'PRES',{},'ATMP',{},'WTMP',{},'DEWP',{},'VIS',{},'TIDE',{},'time',{});
             Obj.expected_units_metH = struct('WDIR',{"degT"},'WSPD',{"m/s"},'GST',{"m/s"},'WVHT',{"m"},'DPD',{"sec"},'APD',{"sec"},'MWD',{"deg"},'PRES',{"hPa"},'ATMP',{"degC"},'WTMP',{"degC"},'DEWP',{"degC"},'VIS',{"nmi"},'TIDE',{"ft"});
             
-            data = read_NDBC_file('../../examples/data/wave/46097h201908qc.txt');
+            relative_file_name = '../../examples/data/wave/46097h201908qc.txt';
+            full_file_name = fullfile(fileparts(mfilename('fullpath')), relative_file_name);
+            data = read_NDBC_file(full_file_name);
             datarm1 = rmfield(data,'units');
             datarm2 = rmfield(datarm1,'time');
             expected_index0 = datestr(datenum(2019,8,1,0,0,0));
@@ -38,7 +42,9 @@ classdef Wave_TestIO < matlab.unittest.TestCase
 
         % Spectral data
         function test_read_NDBC_spectral(testCase)
-            data = read_NDBC_file("../../examples/data/wave/data.txt");
+            relative_file_name = "../../examples/data/wave/data.txt";
+            full_file_name = fullfile(fileparts(mfilename('fullpath')), relative_file_name);
+            data = read_NDBC_file(full_file_name);
             
             assertEqual(testCase,size(data.spectrum),[47 743]);
         end
@@ -65,7 +71,9 @@ classdef Wave_TestIO < matlab.unittest.TestCase
                         "46029w1998.txt.gz"];
             ndbc_data = NDBC_request_data('swden', filenames);
             ndbc_data = struct2table(ndbc_data.ID_46042.year_1996,'AsArray',true);
-            file = gunzip('../../examples/data/wave/46042w1996.txt.gz');
+            relative_file_name = '../../examples/data/wave/46042w1996.txt.gz';
+            full_file_name = fullfile(fileparts(mfilename('fullpath')), relative_file_name);
+            file = gunzip(full_file_name);
             expected_data = readmatrix(file{1});
             temp = table2array(ndbc_data(:,6));
             data_array = table2array(ndbc_data(:,[1,2,3,4]));
@@ -78,28 +86,91 @@ classdef Wave_TestIO < matlab.unittest.TestCase
         end
         
         function test_swan_read_table(testCase)
-            file = '../../examples/data/wave/SWAN/SWANOUT.DAT';
+            relative_file_name = '../../examples/data/wave/SWAN/SWANOUT.DAT';
+            full_file_name = fullfile(fileparts(mfilename('fullpath')), relative_file_name);
             delimiterIn = ' ';
-            mystructure = importdata(file,delimiterIn);
+            mystructure = importdata(full_file_name,delimiterIn);
             vars = string(strsplit(mystructure.textdata{5},' '));
             vars = vars(2:end-1);
             expected = table2struct(array2table(mystructure.data,'VariableNames',vars),'ToScalar',true);
-            data = swan_read_table(file);
+            data = swan_read_table(full_file_name);
             
             assertEqual(testCase,data.Hsig,expected.Hsig);      
         end
         
         function test_swan_read_block(testCase)
-            file = '../../examples/data/wave/SWAN/SWANOUT.DAT';
-            file2 = '../../examples/data/wave/SWAN/SWANOUTBlock.DAT';
+            relative_file_name = '../../examples/data/wave/SWAN/SWANOUT.DAT';
+            relative_file_name2 = '../../examples/data/wave/SWAN/SWANOUTBlock.DAT';
+            full_file_name = fullfile(fileparts(mfilename('fullpath')), relative_file_name);
+            full_file_name2 = fullfile(fileparts(mfilename('fullpath')), relative_file_name2);
             delimiterIn = ' ';
-            mystructure = importdata(file,delimiterIn);
+            mystructure = importdata(full_file_name,delimiterIn);
             vars = string(strsplit(mystructure.textdata{5},' '));
             vars = vars(2:end-1);
             expected = table2struct(array2table(mystructure.data,'VariableNames',vars),'ToScalar',true);
-            data = swan_read_block(file2);
+            data = swan_read_block(full_file_name2);
             
             assertEqual(testCase,sum(sum(data.Significant_wave_height.values)),sum(expected.Hsig),'RelTol',0.001);      
+        end
+        
+        % WPTO multiple locations
+        function test_WPTO_point_multiloc(testCase)
+
+            api_key = '3K3JQbjZmWctY0xmIfSYvYgtIcM3CN0cb1Y2w9bf';
+            hindcast_data = request_wpto('1-hour',...
+                ["energy_period"],[44.624076,-124.280097;43.489171,-125.152137],...
+                2010,api_key);
+            file = '../../examples/data/wave/hindcast/hindcast_1hr_data.csv';
+            meta = '../../examples/data/wave/hindcast/hindcast_1hr_meta.csv';
+            expected_data = readtable(file,'delimiter',',');
+            expected_meta = readtable(meta);
+            expected_data.time_index = datetime(expected_data.time_index,'InputFormat','yyyy-MM-dd HH:mm:ssXXX',...
+                'TimeZone','UTC');
+            
+%            
+            assertEqual(testCase,expected_data.time_index,hindcast_data(1).time);
+            assertEqual(testCase,expected_data.energy_period_0,hindcast_data(1).energy_period,'RelTol',0.000001);
+            assertEqual(testCase,expected_meta.latitude(1),hindcast_data(1).metadata.latitude,'RelTol',0.000001);
+            assertEqual(testCase,expected_meta.longitude(1),hindcast_data(1).metadata.longitude,'RelTol',0.000001);
+            assertEqual(testCase,expected_meta.water_depth(1),hindcast_data(1).metadata.water_depth,'RelTol',0.000001);
+            assertEqual(testCase,expected_meta.timezone(1),hindcast_data(1).metadata.timezone);
+            assertEqual(testCase,expected_meta.jurisdiction{1},hindcast_data(1).metadata.jurisdiction);
+            assertEqual(testCase,expected_meta.distance_to_shore(1),hindcast_data(1).metadata.distance_to_shore,'RelTol',0.000001);
+             
+            assertEqual(testCase,expected_data.time_index,hindcast_data(2).time);
+            assertEqual(testCase,expected_data.energy_period_1,hindcast_data(2).energy_period,'RelTol',0.000001);
+            assertEqual(testCase,expected_meta.latitude(2),hindcast_data(2).metadata.latitude,'RelTol',0.000001);
+            assertEqual(testCase,expected_meta.longitude(2),hindcast_data(2).metadata.longitude,'RelTol',0.000001);
+            assertEqual(testCase,expected_meta.water_depth(2),hindcast_data(2).metadata.water_depth,'RelTol',0.000001);
+            assertEqual(testCase,expected_meta.timezone(2),hindcast_data(2).metadata.timezone);
+            assertEqual(testCase,expected_meta.jurisdiction{2},hindcast_data(2).metadata.jurisdiction);
+            assertEqual(testCase,expected_meta.distance_to_shore(2),hindcast_data(2).metadata.distance_to_shore,'RelTol',0.000001);
+         
+        end
+        
+        function test_WPTO_point_multiparm(testCase)
+
+            api_key = '3K3JQbjZmWctY0xmIfSYvYgtIcM3CN0cb1Y2w9bf';
+            hindcast_data = request_wpto('3-hour',...
+                ["mean_absolute_period","significant_wave_height"],[44.624076,-124.280097],...
+                1996,api_key);
+            file = '../../examples/data/wave/hindcast/hindcast_3hr_data.csv';
+            meta = '../../examples/data/wave/hindcast/hindcast_3hr_meta.csv';
+            expected_data = readtable(file,'delimiter',',');
+            expected_meta = readtable(meta);
+            expected_data.time_index = datetime(expected_data.time_index,'InputFormat','yyyy-MM-dd HH:mm:ssXXX',...
+                'TimeZone','UTC');
+
+%             
+            assertEqual(testCase,expected_data.time_index,hindcast_data.time);
+            assertEqual(testCase,expected_data.mean_absolute_period_0,hindcast_data.mean_absolute_period,'RelTol',0.000001);
+            assertEqual(testCase,expected_data.significant_wave_height_0,hindcast_data.significant_wave_height,'RelTol',0.000001);
+            assertEqual(testCase,expected_meta.latitude(1),hindcast_data.metadata.latitude,'RelTol',0.000001);
+            assertEqual(testCase,expected_meta.longitude(1),hindcast_data.metadata.longitude,'RelTol',0.000001);
+            assertEqual(testCase,expected_meta.water_depth(1),hindcast_data.metadata.water_depth,'RelTol',0.000001);
+            assertEqual(testCase,expected_meta.timezone(1),hindcast_data.metadata.timezone);
+            assertEqual(testCase,expected_meta.jurisdiction{1},hindcast_data.metadata.jurisdiction);
+            assertEqual(testCase,expected_meta.distance_to_shore(1),hindcast_data.metadata.distance_to_shore,'RelTol',0.000001);
         end
     end
 end
