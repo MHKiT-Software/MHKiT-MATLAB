ec93f317629799a51a8017b033447ecec05051fc
Mathew Topper <damm_horse@yahoo.co.uk>
2022-02-04 15:03:30 +0000

    ﻿Speed up reading of dataframe datetimes
    * Add datetime_index_to_ordinal function to pandas_dataframe module to convert the datetime index of dataframes to MATLAB ordinal values
    * Convert all methods using cell arrays to get the datetimes index to use datetime_index_to_ordinal function

--- mhkit/tests/Tidal_TestIO.m
+++ mhkit/tests/Tidal_TestIO.m
@@ -3,6 +3,7 @@ classdef Tidal_TestIO < matlab.unittest.TestCase
     methods (Test) 
         
         function test_load_noaa_data(testCase)
+            
             relative_file_name = '../../examples/data/tidal/s08010.json';
             full_file_name = fullfile(fileparts(mfilename('fullpath')), relative_file_name);
             data = read_noaa_json(full_file_name);
@@ -13,6 +14,7 @@ classdef Tidal_TestIO < matlab.unittest.TestCase
             assertEqual(testCase,size(data.s), [18890 1]);
             assertEqual(testCase,size(data.d), [18890 1]);
             assertEqual(testCase,size(data.b), [18890 1]);
+            
         end
 
         function test_request_noaa_data(testCase)
