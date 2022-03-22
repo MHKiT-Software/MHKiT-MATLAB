2e667304983e4370ada72084638dbb2f8976339c
Matthew Boyd <mr.matthew.boyd@gmail.com>
2022-03-22 13:58:25 -0600

    Reduce query size of USGS instantaneous data test to avoid query limit

--- mhkit/tests/River_TestIO.m
+++ mhkit/tests/River_TestIO.m
@@ -32,10 +32,11 @@ classdef River_TestIO < matlab.unittest.TestCase
         end
 
         function test_request_usgs_data_instant(testCase)
-            data=request_usgs_data("15515500","00060","2009-08-01","2009-08-10","data_type",'Instantaneous');
+            % Querying only one day to avoid USGS query limit and thus failed test
+            data=request_usgs_data("15515500","00060","2009-08-01","2009-08-01","data_type",'Instantaneous');
             
             assertEqual(testCase,data.units, struct('Discharge'," cubic feet per second"));
-            assertEqual(testCase,size(data.Discharge), [10*24*4 1]);
+            assertEqual(testCase,size(data.Discharge), [1*24*4 1]);
         end
     end
 end
