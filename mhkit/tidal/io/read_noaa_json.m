cf77fa3b60f5bbeae9cc40f67d269c14a90b820a
McVey <amcvey@nrel.gov>
2022-04-29 15:39:20 -0400

    trying to fix git issue

--- mhkit/tidal/io/read_noaa_json.m
+++ mhkit/tidal/io/read_noaa_json.m
@@ -19,49 +19,6 @@ function data=read_noaa_json(filename)
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% py.importlib.import_module('mhkit');
-% py.importlib.import_module('numpy');
-% 
-% datap = py.mhkit.tidal.io.noaa.read_noaa_json(filename);
-% 
-% datac=cell(datap);
-% data=struct(datac{2});
-% data_df=datac{1};
-% 
-% fields=fieldnames(data);
-% 
-% for idx = 1:length(fields)
-%     data.(fields{idx}) = string(data.(fields{idx}));
-% end
-% 
-% xx=cell(data_df.axes);
-% v=xx{2};
-% 
-% 
-% vv=cell(py.list(py.numpy.nditer(v.values,pyargs("flags",{"refs_ok"}))));
-% 
-% vals=double(py.array.array('d',py.numpy.nditer(data_df.values,pyargs("flags",{"refs_ok"}))));
-% sha=cell(data_df.values.shape);
-% x=int64(sha{1,1});
-% y=int64(sha{1,2});
-% 
-% vals=reshape(vals,[x,y]);
-% si=size(vals);
-% 
-% for i=1:si(2)
-%     test=string(py.str(vv{i}));
-%     newname=split(test,",");
-%     data.(newname(1))=vals(:,i);
-% end
-%  
-% times = double(    ...
-%      py.mhkit_python_utils.pandas_dataframe.datetime_index_to_ordinal(data_df));
-% 
-% data.time = posixtime(datetime(times, 'ConvertFrom', 'datenum'));
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 fid = fopen(filename);  % Open the file
 raw = fread(fid,inf);   % Read the contents
 str = char(raw');       % Transformation
