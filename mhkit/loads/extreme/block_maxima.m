f915c83aac0c2dadef0ba32569453147a3b32b6e
McVey <amcvey@nrel.gov>
2022-06-16 16:34:20 -0400

    First push for short term extreme. Adding example then moving on to develop relevant functions

---
+++ mhkit/loads/extreme/block_maxima.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = block_maxima(inputArg1,inputArg2)
+%BLOCK_MAXIMA Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
