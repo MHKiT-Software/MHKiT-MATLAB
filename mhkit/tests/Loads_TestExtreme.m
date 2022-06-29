8639964500855ee2bfb67a070c70df2b7efba74c
hivanov-nrel <53838851+hivanov-nrel@users.noreply.github.com>
2022-06-29 14:14:22 -0600

    tests and all MLER functions

--- mhkit/tests/Loads_TestExtreme.m
+++ mhkit/tests/Loads_TestExtreme.m
@@ -13,7 +13,7 @@ classdef Loads_TestExtreme < matlab.unittest.TestCase
 
         function test_mler_coefficients(testCase)
             % create inputs and load validation data
-            fpath = '../../examples/data/loads/mler_data.csv';
+            fpath = '../../examples/data/loads/mler.csv';
             validation = readtable(fpath);
             wave_freq = linspace(0,1,500);
             js = jonswap_spectrum(wave_freq,15.1,9);
@@ -26,6 +26,48 @@ classdef Loads_TestExtreme < matlab.unittest.TestCase
             assertEqual(testCase, mler.phase, validation.phase, 'RelTol',0.001)
         end
 
-
+        function test_mler_simulation(testCase)
+            % validation data
+            T = linspace(-150,150,301);
+            X = linspace(-300,300,601);
+            sim = mler_simulation();
+            
+            assertEqual(testCase, sim.T, T)
+            assertEqual(testCase, sim.X, X)
+        end
+        
+        function test_mler_wave_amp_normalize(testCase)
+            fpath = '../../examples/data/loads/mler.csv';
+            validation = readtable(fpath);
+            mler.conditioned_spectrum = validation.Res_Spec;
+            mler.phase = validation.phase;
+            wave_freq = linspace(0,1,500);
+            mler.frequency = wave_freq;
+            
+            k = wave_number(wave_freq, 70);
+            k.values = fillmissing(k.values,'constant',0);
+            sim = mler_simulation();
+            mler_norm = mler_wave_amp_normalize(4.5*1.9, mler, sim, k.values);
+            
+            assertEqual(testCase, mler_norm.conditioned_spectrum, validation.Norm_Spec, 'AbsTol', 0.002)
+        end
+        
+        function test_mler_export_time_series(testCase)
+            fpath = '../../examples/data/loads/mler_ts.csv';
+            validation = readtable(fpath);
+            fpath2 = '../../examples/data/loads/mler.csv';
+            normed = readtable(fpath2);
+            mler.conditioned_spectrum = normed.Norm_Spec;
+            mler.phase = normed.phase;
+            wave_freq = linspace(0,1,500);
+            mler.frequency = wave_freq;
+            RAO = normed.RAO;
+            k = wave_number(wave_freq, 70);
+            k.values = fillmissing(k.values,'constant',0); 
+            sim = mler_simulation();
+            mler_ts = mler_export_time_series(RAO, mler, sim, k.values);
+            
+            assertEqual(testCase, mler_ts.linear_response, validation.LinearResponse, 'AbsTol', 0.00005)
+         end
     end
 end
