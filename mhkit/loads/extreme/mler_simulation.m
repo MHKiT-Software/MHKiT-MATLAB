6be98b5a72ef93fbaea50e72036325f0c8489edf
Chris-Ivanov <53838851+hivanov-nrel@users.noreply.github.com>
2022-11-17 09:16:35 -0700

    WDRT functionality: MLER (#96)
    * initial mler commit
    
    * mler_coefficients function
    
    * tests and all MLER functions
    
    * mler bug fixes
    
    * create mler example
    
    * updates from review

---
+++ mhkit/loads/extreme/mler_simulation.m
@@ -0,0 +1,55 @@
+function sim = mler_simulation(sim)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%     Define the simulation parameters that are used in various MLER
+%     functionalities. If no input is given, then default values are returned.  
+%     
+%     Parameters
+%     ----------
+%         sim : struct (optional)
+%             Simulation parameters
+%             Keys:
+%             -----
+%             'startTime': starting time [s]
+%             'endTime': ending time [s]
+%             'dT': time-step size [s]
+%             'T0': time of maximum event [s]
+%             'startX': start of simulation space [m]
+%             'endX': end of simulation space [m]
+%             'dX': horizontal spacing [m]
+%             'X0': position of maximum event [m]
+%         
+%      Returns
+%      -------
+%         sim : struct 
+%             Simulation parameters including spatial and time calculated
+%             arrays
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+if nargin < 1
+    sim.startTime = -150.0;  % .s Starting time
+    sim.endTime = 150.0;  % .s Ending time
+    sim.dT = 1.0;  % .s Time-step size
+    sim.T0 = 0.0;  % .s Time of maximum event
+
+    sim.startX = -300.0;  % .m Start of simulation space
+    sim.endX = 300.0;  % .m End of simulation space
+    sim.dX = 1.0;  % .m Horiontal spacing
+    sim.X0 = 0.0;  % .m Position of maximum event
+else
+    assert(isa(sim,'struct'), 'sim needs to be of type struct!')
+end 
+
+sim.maxIT = ceil((sim.endTime - sim.startTime)/sim.dT + 1);
+sim.T = linspace(sim.startTime, sim.endTime, sim.maxIT);
+
+sim.maxIX = ceil((sim.endX - sim.startX)/sim.dX + 1);
+sim.X = linspace(sim.startX, sim.endX, sim.maxIX);
+
+
+end
+
+
