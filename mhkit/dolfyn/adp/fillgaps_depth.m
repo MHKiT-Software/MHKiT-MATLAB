2cd4004161692d647ee1289a69337d8fcf474405
Zuriah Quinton <89942742+zur-quin@users.noreply.github.com>
2023-03-21 12:43:54 -0700

    Update fillgaps_time and fillgaps_depth
    -update these functions to interpolate over time and depth respectively
    -using fillmissing function in matlab now to specify an interpolation dimension
    -include maxgap option for both above functions
    -update comments for above functions

--- mhkit/dolfyn/adp/fillgaps_depth.m
+++ mhkit/dolfyn/adp/fillgaps_depth.m
@@ -8,6 +8,10 @@ function out = fillgaps_depth(var, options)
 %   The variable to clean
 % method : char
 %   Interpolation method to use. Default is 'spline'
+%   see fillmissing method for other options 
+%   https://www.mathworks.com/help/matlab/ref/fillmissing.html#bva1z1c-method
+% options.maxgap : double 
+%   Maximum gap of missing data to interpolate across. Default is None
 %
 % Returns
 % -------
@@ -16,8 +20,6 @@ function out = fillgaps_depth(var, options)
 %
 % See Also
 % --------
-% Methodology for interpolating across nans from 
-% https://www.mathworks.com/matlabcentral/answers/34346-interpolating-nan-s
 % create_dataset.m function
 
 % TODO: need to assert that var is data struct from create_dataset here
@@ -25,6 +27,7 @@ function out = fillgaps_depth(var, options)
     arguments
         var;
         options.method char = 'spline';
+        options.maxgap double = NaN;
     end
 
     range_dim = 0;
@@ -42,18 +45,13 @@ function out = fillgaps_depth(var, options)
         error('No range dimension found')
     end
 
-    % now interpolate the nans away
-
-    % get array of true false for which are nans in var.data
-    % not_nans has 0 if nan and 1 if number (since ~)
-    nans = isnan(var.data);
-    not_nans = ~nans;
-    % t is the array {1,2,3,....,num_elements_in_var.data}
-    t = 1:numel(var.data); 
-    % output is var except the nans have been interpolated:
+    % now interpolate the nans away, note fillmissing will look for NaNs in
+    % var.data as the missing entries
     out = var;
-    % reassign any nans in out.data to be interpolated values at the nan
-    % positions
-    % TODO: interploate over just range dim
-    out.data(nans) = interp1(t(not_nans), var.data(not_nans), t(nans), options.method);
-end
+    if isnan(options.maxgap) %if maxgap is Nan, no maxgap was set:
+        out.data = fillmissing(var.data,options.method,range_dim);
+    end
+    if ~isnan(options.maxgap) % if maxgap is not NaN, a maxgap is set:
+        out.data = fillmissing(var.data,options.method,range_dim,MaxGap=options.maxgap);
+    end
+    end
--- mhkit/dolfyn/adp/fillgaps_time.m
+++ mhkit/dolfyn/adp/fillgaps_time.m
@@ -6,8 +6,12 @@ function out = fillgaps_time(var, options)
 % ----------
 % var : struct (from a fieldname in dataset in create_dataset function)
 %   The variable to clean
-% method : char
+% options.method : char
 %   Interpolation method to use. Default is 'spline'
+%   see fillmissing method for other options 
+%   https://www.mathworks.com/help/matlab/ref/fillmissing.html#bva1z1c-method
+% options.maxgap : double 
+%   Maximum gap of missing data to interpolate across. Default is None
 %
 % Returns
 % -------
@@ -16,8 +20,6 @@ function out = fillgaps_time(var, options)
 %
 % See Also
 % --------
-% Methodology for interpolating across nans from 
-% https://www.mathworks.com/matlabcentral/answers/34346-interpolating-nan-s
 % create_dataset.m function
 
 % TODO: need to assert that var is data struct from create_dataset here
@@ -25,6 +27,7 @@ function out = fillgaps_time(var, options)
     arguments
         var;
         options.method char = 'spline';
+        options.maxgap double = NaN;
     end
 
     time_dim = 0;
@@ -43,17 +46,11 @@ function out = fillgaps_time(var, options)
     end
 
     % now interpolate the nans away
-
-    % get array of true false for which are nans in var.data
-    % not_nans has 0 if nan and 1 if number (since ~)
-    nans = isnan(var.data);
-    not_nans = ~nans;
-    % t is the array {1,2,3,....,num_elements_in_var.data}
-    t = 1:numel(var.data); 
-    % output is var except the nans have been interpolated:
     out = var;
-    % reassign any nans in out.data to be interpolated values at the nan
-    % positions
-    % TODO: interpolate over just time_dim
-    out.data(nans) = interp1(t(not_nans), var.data(not_nans), t(nans), options.method);
+    if isnan(options.maxgap) %if maxgap is Nan, no maxgap was set:
+        out.data = fillmissing(var.data,options.method,time_dim);
+    end
+    if ~isnan(options.maxgap) % if maxgap is not NaN, a maxgap is set:
+        out.data = fillmissing(var.data,options.method,time_dim,MaxGap=options.maxgap);
+    end
 end
