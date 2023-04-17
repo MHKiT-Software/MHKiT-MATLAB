ffd4b59612482659801139b7ac66451c9d8595de
Zuriah Quinton <89942742+zur-quin@users.noreply.github.com>
2023-04-17 16:07:23 -0700

    Update fillgaps_time
    -fillgaps_time now checks var's type a little, and also uses correct error syntax
    -Note: MATLAB's fillmissing function does not have a default option for MaxGap, so if statement is still used for MaxGap argument

--- mhkit/dolfyn/adp/fillgaps_time.m
+++ mhkit/dolfyn/adp/fillgaps_time.m
@@ -22,12 +22,23 @@ function out = fillgaps_time(var, options)
 % --------
 % create_dataset.m function
 
-% TODO: need to assert that var is data struct from create_dataset here
 % set default value for options.method to 'spline'
     arguments
         var;
         options.method char = 'spline';
         options.maxgap double = NaN;
+    end
+    % Make sure that var is a structure
+    if ~isstruct(var)
+        ME = MException('MATLAB:fillgaps_time','var must be a structure');
+        throw(ME);
+    end
+
+    % Make sure that ds contains the dolyn fields
+    if ~isfield(var,'coords') || ~isfield(var,'data') 
+        ME = MException('MATLAB:fillgaps_time',['The provided data ' ...
+            'structure does not appear to have been created by dolfyn']);
+        throw(ME);
     end
 
     time_dim = 0;
@@ -40,9 +51,10 @@ function out = fillgaps_time(var, options)
        end
     end
 
+    % if time_dim is still 0, then no time_dim was found in var.dims
     if time_dim == 0
-        % this means prev loop didnt find 'time' in any dims -> throw error
-        error('No time dimension found')
+        ME = MException('MATLAB:fillgaps_time',['No time dimension found']);
+        throw(ME);
     end
 
     % now interpolate the nans away
@@ -51,6 +63,6 @@ function out = fillgaps_time(var, options)
         out.data = fillmissing(var.data,options.method,time_dim);
     end
     if ~isnan(options.maxgap) % if maxgap is not NaN, a maxgap is set:
-        out.data = fillmissing(var.data,options.method,time_dim,MaxGap=options.maxgap);
+        out.data = fillmissing(var.data,options.method,time_dim,'MaxGap', options.maxgap);
     end
 end
