f704dc6e6c8d41ad610300e9b01362a9ee562f01
Andrew Simms <simms.andrew@gmail.com>
2023-11-07 11:44:54 -0700

    WIP: Tests: Add more python tests

--- mhkit/tests/APython_MHKiT_Import.m
+++ mhkit/tests/APython_MHKiT_Import.m
@@ -7,19 +7,104 @@ classdef APython_MHKiT_Import < matlab.unittest.TestCase
             assertTrue(testCase, true);
         end
 
+        % Test passing a list of lists to python, then reading the result
+        % in python
+        % Expected output is the same as the input. The final argument to
+        % timeseries_to_pandas determines whethe
+        % function test_convert_timeseries_to_dataframe_without_transpose(testCase)
+        %     time_series = { [1, 2, 3], [4, 5, 6] };
+        %     disp(class(time_series));
+        %     index = {'2023-01-01', '2023-01-02'};
+        %     disp(class(index));
+        %     datetime_index = datetime(index, 'InputFormat', 'yyyy-MM-dd');
+        %     disp(datetime_index);
+        %     disp(class(datetime_index));
+        %     disp(length(datetime_index));
+        %     % py.mhkit_python_utils.pandas_dataframe.convert_timeseries_to_dataframe(time_series, index)
+        %     df = py.mhkit_python_utils.pandas_dataframe.timeseries_to_pandas(time_series, datetime_index, 1);
+        %     % disp(result.values);
+        %     % disp(result.index.values);
+        %     % disp(class(result.index.values));
+        % 
+        %     if (isa(df,'py.pandas.core.frame.DataFrame')==1)
+        %         % disp('Is a dataframe!')
+        %         % disp(result.values);
+        %         % disp(class(result.values));
+        %         % details(result.values);
+        %         assertEqual(testCase, double(df.values.ndim), 2);
+        %         assertEqual(testCase, double(df.index.size), 2);
+        % 
+        %         df_column_count = size(df.values, 2);
+        %         data=py.list();
+        %         if df_column_count > 1
+        %             for i = 1:x(2)
+        %                 app=py.list(result.values(:,i));
+        %                 data=py.mhkit_python_utils.pandas_dataframe.lis(data,app);
+        %             end
+        %         elseif df_column_count == 1
+        %             data=df.values;
+        %         end
+        % 
+        %         disp(df.index.values);
+        %         disp(class(df.index.values));
+        %         % cleaned_index = cellstr(strsplit(df.index.values));
+        %         % assertEqual(testCase, index, cleaned_index);
+        %         % df_index = cellstr(df.index.values);
+        % 
+        %         % Unpack the values
+        %         % disp(data);
+        %         cleaned_result = double(data);
+        %        %  disp(cleaned_result);
+        %        % 
+        %        % disp(class(cleaned_result));
+        %        % disp(cleaned_result(1, :));
+        %        % disp(cleaned_result(2, :));
+        % 
+        %        assertEqual(testCase, time_series{1}, cleaned_result(1, :));
+        %        assertEqual(testCase, time_series{2}, cleaned_result(2, :));
+        % 
+        %     else
+        %         verifyFail(testCase);
+        %     end
+        % 
+        %     assertTrue(testCase, true);
+        % end
+
+        function test_data_conversion_matlab_to_python_to_matlab(testCase)
+         time_series = { [1, 2, 3], [4, 5, 6] };
+            index = {'2023-01-01', '2023-01-02'};
+            result = py.mhkit_python_utils.pandas_dataframe.timeseries_to_pandas(time_series, index, 1);
+            % disp(result.values);
+            % disp(result.index.values);
+            % disp(class(result.index.values));
+
+            if (isa(result,'py.pandas.core.frame.DataFrame')==1)
+                % disp('Is a dataframe!')
+                % disp(result.values);
+                % disp(class(result.values));
+                % details(result.values);
+                assertEqual(testCase, double(result.values.ndim), 2);
+                assertEqual(testCase, double(result.index.size), 2);
+            else
+                verifyFail(testCase);
+            end
+
+            assertTrue(testCase, true);
+        end
+
         function test_mhkit_import(testCase)
-            pyenv()
-            getenv('PYTHONHOME')
-            getenv("PATH")
             py.importlib.import_module('mhkit');
             assertTrue(testCase, true);
         end
 
         function verify_mhkit_output(testCase)
             [x,y] = circular(30);
-            
+
+            disp(x);
+            disp(y);
+
             expected_x = 30;
-            expected_y = 11309.733552;
+            expected_y = 706.8583;
 
             y_variance = abs(y - expected_y);
 
