e2613e434fc97ca6c2e47e61aea3840990496c55
Andrew Simms <simms.andrew@gmail.com>
2023-11-28 16:00:13 -0700

    Tests: Fix test_request_noaa_data
    The function request_noaa_data_restricted_duration was erroneously
    deleted. This adds back that function unmodified from https://github.com/MHKiT-Software/MHKiT-MATLAB/blob/deedd7e7099de21ef742877f3de07af2ec657745/mhkit/tidal/io/request_noaa_data.m

--- mhkit/tidal/io/request_noaa_data.m
+++ mhkit/tidal/io/request_noaa_data.m
@@ -90,6 +90,145 @@ while start_period_datetime <= end_datetime
     elseif is_first_query
         data = data_in_period;
         is_first_query = false;
+    else
+        % Append to existing data structure
+        for i = 1:length(timeseries_fields)
+            data.(timeseries_fields{i}) = cat( ...
+                1, ...  % by rows
+                data.(timeseries_fields{i}), ...
+                data_in_period.(timeseries_fields{i}));
+        end
+    end
+
+    start_period_datetime = end_period_datetime + days(1);
+end
+
+% Write NOAA data to a JSON file
+if options.write_json ~= ""
+    fid = fopen(options.write_json, 'w');
+    fprintf(fid, jsonencode(data));
+end
+
+end
+
+function [data, timeseries_fields] = request_noaa_data_restricted_duration( ...
+        station, parameter, start_date, end_date, ...
+        MAX_RETRIES, REQUIRED_FIELDS, MAX_DAYS_PER_QUERY)
+    % Verify dates are less than MAX_DAYS_PER_QUERY apart
+    start_datetime = datetime(start_date, ...
+        'InputFormat', 'yyyyMMdd', ...
+        'TimeZone', 'UTC');
+    end_datetime = datetime(end_date, ...
+        'InputFormat', 'yyyyMMdd', ...
+        'TimeZone', 'UTC');
+    msg = 'Date range is greater than %d days in request_noaa_data_restricted_duration()';
+    assert(days(end_datetime - start_datetime) + 1 <= MAX_DAYS_PER_QUERY, ...
+        sprintf(msg, MAX_DAYS_PER_QUERY));
+
+    % Formulate query
+    start_date = convertCharsToStrings(start_date);
+    start_date = convertCharsToStrings(start_date);
+    start_date = convertCharsToStrings(start_date);
+    end_date = convertCharsToStrings(end_date);
+    data_url = "https://tidesandcurrents.noaa.gov/api/datagetter?";
+    api_query = "begin_date=" + start_date ...
+                + "&end_date=" + end_date ...
+                + "&station=" + station ...
+                + "&product=" + parameter ...
+                + "&units=metric&" ...
+                + "time_zone=gmt&" ...
+                + "application=web_services&" ...
+                + "format=xml";
+
+    % Display query
+    disp("Data request URL: " + data_url + api_query)
+
+    % Submit query and get data
+    for i = 0:MAX_RETRIES
+        try
+            response = webread(data_url + api_query);
+            break;
+        catch ME
+            if i == MAX_RETRIES
+                disp(['MATLAB:request_noaa_data: ', ME.identifier]);
+                rethrow(ME)
+            else
+                pause(1);   % pause(seconds) and retry query
+            end
+        end
+    end
+
+    % Organize a structure containing the metadata and timeseries data
+    is_metadata_found = false;
+    is_timeseries_keys_found = false;
+    data_lines = strsplit(response, '\n');
+    for i = 1:length(data_lines)
+        if startsWith(data_lines{i}, '<metadata')
+            % Parse metadata and initialize output structure using them
+            if ~is_metadata_found
+                % Note: only key names of one or more letters work
+                meta_names = regexp(data_lines{i}, '[a-zA-Z]+(?=\=)', 'match');
+                for j = 1:length(meta_names)
+                    regex_pattern = strcat('(?<=', meta_names{j}, '\=")(.*?)(?=")');
+                    value = regexp(data_lines{i}, regex_pattern, 'match');
+                    if meta_names{j} == "lat" || meta_names{j} == "lon"
+                        data.(meta_names{j}) = str2double(value{1});
+                    else
+                        data.(meta_names{j}) = convertCharsToStrings(value{1});
+                    end
+                end
+                is_metadata_found = true;
+
+                % Verify all required metadata is present
+                for j = 1:length(REQUIRED_FIELDS)
+                    if ~isfield(data, REQUIRED_FIELDS{j})
+                        ME = MException('MATLAB:request_noaa_data', ...
+                            strcat('Queried data from NOAA is missing field', ...
+                                REQUIRED_FIELDS{j}));
+                    end
+                end
+            end
+        elseif startsWith(data_lines{i}, '<cu')
+            % Parse timeseries data keys and add to output structure
+            if ~is_timeseries_keys_found
+                % Note: only key names of one or more letters work
+                ts_keys = regexp(data_lines{i}, '[a-zA-Z]+(?=\=)', 'match');
+                timeseries_fields = [];
+                for j = 1:length(ts_keys)
+                    if ts_keys{j} == 't'
+                        ts_field_struct.(ts_keys{j}) = 'time';
+                    else
+                        ts_field_struct.(ts_keys{j}) = ts_keys{j};
+                    end
+                    timeseries_fields = [timeseries_fields, ...
+                        convertCharsToStrings(ts_field_struct.(ts_keys{j}))];    % to return
+                    data.(ts_field_struct.(ts_keys{j})) = [];
+                end
+                is_timeseries_keys_found = true;
+            end
+
+            % Parse data from current line and add to respective arrays in structure
+            for j = 1:length(ts_keys)
+                regex_pattern = strcat('(?<=', ts_keys{j}, '\=")(.*?)(?=")');
+                value = regexp(data_lines{i}, regex_pattern, 'match');
+                if ts_keys{j} == 't'
+                    value{1} = posixtime(datetime( ...
+                        value{1}, ...
+                        'InputFormat', 'yyyy-MM-dd HH:mm', ...
+                        'TimeZone', 'UTC'));
+                else
+                    value{1} = str2double(value{1});
+                end
+                data.(ts_field_struct.(ts_keys{j}))(end+1, :) = value{1};
+            end
+        elseif startsWith(data_lines{i}, '<error')
+            noaa_error_msg = regexp(data_lines{i}, '(?<=.error.).*(?=..error.)', 'match');
+            warning('For station id %s between %s and %s, NOAA returned the error:\n%s', ...
+                station, start_date, end_date, noaa_error_msg{1});
+            data = struct;      % empty structure
+            timeseries_fields = [];
+            break
+        end
     end
 end
 
