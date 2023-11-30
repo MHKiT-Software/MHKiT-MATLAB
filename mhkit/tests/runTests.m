1f5f93af9d4c8afdc98af91eed963ea8b9aa1e59
Andrew Simms <simms.andrew@gmail.com>
2023-11-30 13:47:05 -0700

    Tests: Add code coverage
    Use built in MATLAB code coverage tool to generate code coverage report.
    
    There are two warnings when this runs for which fixes are not readily
    apparent:
    
    Warning: Coverage result is empty for check_range.m because the file is invalid.
    
    Warning: Coverage result is empty for blade_moments.m because the file is invalid.
    
    These two warnings indicate that code coverage is not being generated
    for those files.

--- mhkit/tests/runTests.m
+++ mhkit/tests/runTests.m
@@ -3,6 +3,9 @@ function results = runTests()
     import matlab.unittest.TestRunner;
     import matlab.unittest.TestSuite;
     import matlab.unittest.plugins.TestReportPlugin;
+    import matlab.unittest.plugins.CodeCoveragePlugin;
+    import matlab.unittest.plugins.codecoverage.CoverageReport;
+
 
     % Define test suite
     testFileName = mfilename('fullpath');
@@ -21,6 +24,14 @@ function results = runTests()
     pdfFile = fullfile(htmlFolder, 'test_results.pdf');
     plugin = TestReportPlugin.producingPDF(pdfFile);
     runner.addPlugin(plugin);
+
+    % Add Code Coverage Report
+    sourceCodeFolder = fileparts(testsFolder);
+    codeCoverageFolder = fullfile(testsFolder, 'coverage_report');
+    reportFormat = CoverageReport(codeCoverageFolder);
+
+    p = CodeCoveragePlugin.forFolder(sourceCodeFolder, Producing=reportFormat, IncludingSubfolders=true);
+    runner.addPlugin(p);
 
     % Run the tests
     results = runner.run(suite);
