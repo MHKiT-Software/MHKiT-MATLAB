00efc7222eaac51710ca928664660187d6d023fe
Andrew Simms <simms.andrew@gmail.com>
2023-12-05 12:36:14 -0700

    Tests: Use mkhit without explicit import

--- mhkit/tests/Python_Import.m
+++ mhkit/tests/Python_Import.m
@@ -23,6 +23,21 @@ classdef Python_Import < matlab.unittest.TestCase
 
         % Verify output of the 'circular' function from 'mhkit'
         function verify_mhkit_output(testCase)
+            py.importlib.import_module('mhkit');
+            [x, y] = circular(30);
+
+            expected_x = 30;
+            expected_y = 706.8583;
+
+            y_variance = abs(y - expected_y);
+
+            assertEqual(testCase, x, expected_x);
+            assertLessThan(testCase, y_variance, 0.01);
+        end
+
+        % Verify output of the 'circular' function from 'mhkit'
+        function verify_mhkit_output_without_importing_module(testCase)
+            % py.importlib.import_module('mhkit');
             [x, y] = circular(30);
 
             expected_x = 30;
