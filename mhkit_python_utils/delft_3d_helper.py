3c9edf68565f8545bdb7b5e1f76f87ccbbacb039
Andrew Simms <simms.andrew@gmail.com>
2024-03-05 14:47:37 -0700

    D3D: Add delft_3d_get_keys
    Key extraction for a netCDF4 object in Python is natively supported.
    Within MATLAB nested key extraction is much more difficult due to nested
    structures. With additional effort this could be written in MATLAB.
    
    In this code we extract the keys and values in Python and create two different lists that we pass to MATLAB. The lists are parsed into a struct that is returned to the user.

--- mhkit_python_utils/delft_3d_helper.py
+++ mhkit_python_utils/delft_3d_helper.py
@@ -20,3 +20,25 @@ def open_netcdf_file(filename):
         raise FileNotFoundError(f"The file '{filename}' does not exist.")
 
     return netCDF4.Dataset(filename)
+
+
+def get_d3d_keys(netcdf_root):
+    """
+    Extracts variable keys and their corresponding long names from a Delft3D NetCDF root object.
+
+    Args:
+        netcdf_root (netCDF4.Dataset): The root object of a Delft3D NetCDF dataset.
+
+    Returns:
+        Tuple[list, list]: A tuple containing two lists - the first list contains variable keys,
+        and the second list contains their corresponding long names. If a variable does not have a
+        long name, an empty string is returned instead.
+    """
+    result = {}
+    for var in netcdf_root.variables.keys():
+        try:
+            result[var] = str(netcdf_root[var].long_name)
+        except AttributeError:
+            result[var] = ""
+
+    return [list(result.keys()), list(result.values())]
