fdf2632c467458c597db00461c60b725970b1ad4
Andrew Simms <simms.andrew@gmail.com>
2024-03-05 14:46:32 -0700

    Test: delft_3d_get_keys

--- mhkit/tests/River_TestIO_Delft.m
+++ mhkit/tests/River_TestIO_Delft.m
@@ -8,6 +8,15 @@ classdef River_TestIO_Delft < matlab.unittest.TestCase
             testCase.assertEqual(class(data), 'py.netCDF4._netCDF4.Dataset');
         end
 
+        function testGetKeys(testCase)
+            d3d_file = "../../examples/data/river/d3d/turbineTest_map.nc";
+            data = delft_3d_open_netcdf(d3d_file);
+            data_struct = delft_3d_get_keys(data);
+            num_fieldnames = length(fieldnames(data_struct));
+            testCase.assertEqual(data_struct.s1, 'water level');
+            testCase.assertEqual(num_fieldnames, 52);
+        end
+
     end
 
 end
