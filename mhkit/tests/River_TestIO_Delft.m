106d62607748de67f648eb82f648f8acc5d300f7
Andrew Simms <simms.andrew@gmail.com>
2024-03-06 08:18:54 -0700

    Tests: add delft_3d_get_all_data_points test

--- mhkit/tests/River_TestIO_Delft.m
+++ mhkit/tests/River_TestIO_Delft.m
@@ -48,6 +48,32 @@ classdef River_TestIO_Delft < matlab.unittest.TestCase
             testCase.assertEqual(closest_time_index, expected_index);
         end
 
+        function testGetAllDataPoints(testCase)
+            d3d_file = "../../examples/data/river/d3d/turbineTest_map.nc";
+            data = delft_3d_open_netcdf(d3d_file);
+            run_timestamps = delft_3d_get_all_time(data);
+
+            x_velocity_key = "ucx";
+            seconds_into_simulation = run_timestamps(1, 4);
+            seconds_into_simulation_index = delft_3d_convert_time(data, seconds_into_simulation);
+
+            x_velocity_point_data = delft_3d_get_all_data_points(data, x_velocity_key, seconds_into_simulation_index);
+
+            % Fieldnames
+            expected_fieldnames = 7;
+            num_fieldnames = length(fieldnames(x_velocity_point_data));
+            testCase.assertEqual(num_fieldnames, expected_fieldnames);
+
+            % Value Spot Check
+            expected_index = 2;
+            expected_time = 240;
+            expected_y = 1.125;
+
+            testCase.assertEqual(x_velocity_point_data.time(1, expected_index), expected_time);
+            testCase.assertEqual(x_velocity_point_data.y(1, expected_index), expected_y);
+
+        end
+
     end
 
 end
