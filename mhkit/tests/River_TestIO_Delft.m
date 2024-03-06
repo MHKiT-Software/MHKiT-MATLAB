7153860f4b39a5baef6a92685db974db598defa0
Andrew Simms <simms.andrew@gmail.com>
2024-03-06 12:04:00 -0700

    Test: delft_3d_interpolate_to_centerline

--- mhkit/tests/River_TestIO_Delft.m
+++ mhkit/tests/River_TestIO_Delft.m
@@ -115,6 +115,44 @@ classdef River_TestIO_Delft < matlab.unittest.TestCase
 
         end
 
+        function testInterpolateToCenterline(testCase)
+            d3d_file = "../../examples/data/river/d3d/turbineTest_map.nc";
+            data = delft_3d_open_netcdf(d3d_file);
+            run_timestamps = delft_3d_get_all_time(data);
+
+            x_velocity_key = "ucx";
+            seconds_into_simulation = run_timestamps(1, 4);
+            seconds_into_simulation_index = delft_3d_convert_time(data, seconds_into_simulation);
+
+            x_velocity_point_data = delft_3d_get_all_data_points(data, x_velocity_key, seconds_into_simulation_index);
+
+            xmax = min(x_velocity_point_data.x);
+            xmin = max(x_velocity_point_data.x);
+
+            ymax = min(x_velocity_point_data.y);
+            ymin = max(x_velocity_point_data.y);
+
+            waterdepth_max = min(x_velocity_point_data.waterdepth);
+            waterdepth_min = max(x_velocity_point_data.waterdepth);
+
+            x = linspace(xmin, xmax, 50); % 50 is the numpy default
+            y = mean([ymax, ymin]);
+            waterdepth = mean([waterdepth_max, waterdepth_min]);
+
+            centerline_points = delft_3d_create_points(x, y, waterdepth);
+
+            points = [x_velocity_point_data.x; x_velocity_point_data.y; x_velocity_point_data.waterdepth];
+            values = x_velocity_point_data.(x_velocity_key);
+            xi = [centerline_points.x; centerline_points.y; centerline_points.waterdepth];
+
+            centerline_variable = delft_3d_interpolate_to_centerline(points, values, xi);
+
+            % Value Spot Check
+            expected_index = 3;
+            expected_val = 0.9799;
+            testCase.assertEqual(centerline_variable(1, expected_index), expected_val, 'AbsTol',0.01);
+        end
+
     end
 
 end
