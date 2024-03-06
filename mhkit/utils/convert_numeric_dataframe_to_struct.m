6b2d9d82f0d7645a922a65f4b45a18b93993fab2
Andrew Simms <simms.andrew@gmail.com>
2024-03-06 10:52:48 -0700

    Fix: Python to MATLAB string array conversion
    Conversion from a py.list into a `string` array fails in MATLAB versions
    older than 2022a.

--- mhkit/utils/convert_numeric_dataframe_to_struct.m
+++ mhkit/utils/convert_numeric_dataframe_to_struct.m
@@ -17,11 +17,11 @@ function result_struct = convert_numeric_dataframe_to_struct(df)
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-    columns = string(py.list(df.columns.values));
+    columns = cell(df.columns.values.tolist());
     result_struct = struct();
 
     for i = 1:length(columns)
-        this_column = columns{i};
+        this_column = char(columns{i});
         result_struct.(this_column) = double(df.get(this_column).values.tolist());
     end
 end
