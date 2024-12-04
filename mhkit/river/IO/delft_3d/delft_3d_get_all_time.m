5106d0f4c5ae7f6fde1492f305ef019a9de94fba
Andrew Simms <andrew.simms@nrel.gov>
2024-12-04 11:31:31 -0700

    Actions: Debug `d3d.get_all_time` result output

--- mhkit/river/IO/delft_3d/delft_3d_get_all_time.m
+++ mhkit/river/IO/delft_3d/delft_3d_get_all_time.m
@@ -23,6 +23,8 @@ end
 
     python_result = py.mhkit.river.io.d3d.get_all_time(delft_3d_py_object);
 
+    disp(class(python_result));
+    disp(python_result.shape);
     disp(python_result);
 
     python_result = py.numpy.ndarray(python_result);
