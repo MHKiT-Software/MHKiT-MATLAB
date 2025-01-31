b34008fa45560e1562acb14292df848362585e67
Andrew Simms <andrew.simms@nrel.gov>
2025-01-31 06:33:58 -0700

    Wave: Typecast surface elevation result

--- mhkit/wave/resource/elevation_spectrum.m
+++ mhkit/wave/resource/elevation_spectrum.m
@@ -2,7 +2,7 @@ function S=elevation_spectrum(ts,sample_rate,nnft,time,options)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %   Calculates wave spectra from wave probe timeseries
-%    
+%
 % Parameters
 % ------------
 %     ts: matrix or table
@@ -19,12 +19,12 @@ function S=elevation_spectrum(ts,sample_rate,nnft,time,options)
 %           time (s)
 %
 %     window: string (Optional)
-%        Signal window type. "hamming" is used by default given the broadband 
+%        Signal window type. "hamming" is used by default given the broadband
 %        nature of waves. See scipy.signal.get_window for more options.
 %        to call: elevation_spectrum(ts,sample_rate,nnft,time,"window",window)
 %
 %     detrend: logical (Optional)
-%        Specifies if a linear trend is removed from the data before calculating 
+%        Specifies if a linear trend is removed from the data before calculating
 %        the wave energy spectrum.  Data is detrended by default.
 %        to call: elevation_spectrum(ts,sample_rate,nnft,time,"detrend",detrend)
 %
@@ -38,11 +38,11 @@ function S=elevation_spectrum(ts,sample_rate,nnft,time,options)
 %     S: structure
 %
 %
-%         S.spectrum: vector or matrix Spectral Density (m^2/Hz) per probe           
+%         S.spectrum: vector or matrix Spectral Density (m^2/Hz) per probe
 %
 %         S.type: 'Spectra from Time Series'
 %
-%         S.frequency: frequency [Hz] 
+%         S.frequency: frequency [Hz]
 %
 %         S.sample_rate: sample_rate
 %
@@ -51,7 +51,7 @@ function S=elevation_spectrum(ts,sample_rate,nnft,time,options)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 arguments
-    ts 
+    ts
     sample_rate
     nnft
     time
@@ -60,45 +60,48 @@ arguments
     options.noverlap = py.None;
 end
 
-if (isa(ts,'py.pandas.core.frame.DataFrame')~=1)
-    if (isa(ts,'table')==1)
-        ts=table2array(ts);
-    end
-    if (isa(time,'table')==1)
-        time=table2array(time);
-    end
-    x=size(ts);
-    li=py.list();
-    if x(2)>1 
-        for i = 1:x(2)
-            app=py.list(ts(:,i));
-            li=py.mhkit_python_utils.pandas_dataframe.lis(li,app);
-        end  
-        ts=py.mhkit_python_utils.pandas_dataframe.timeseries_to_pandas(li,time,int32(x(2)));        
+% if (isa(ts,'py.pandas.core.frame.DataFrame')~=1)
+%     if (isa(ts,'table')==1)
+%         ts=table2array(ts);
+%     end
+%     if (isa(time,'table')==1)
+%         time=table2array(time);
+%     end
+%     x=size(ts);
+%     li=py.list();
+%     if x(2)>1
+%         for i = 1:x(2)
+%             app=py.list(ts(:,i));
+%             li=py.mhkit_python_utils.pandas_dataframe.lis(li,app);
+%         end
+%         ts=py.mhkit_python_utils.pandas_dataframe.timeseries_to_pandas(li,time,int32(x(2)));
+%
+%     elseif x(2)==1
+%        ts=py.mhkit_python_utils.pandas_dataframe.timeseries_to_pandas(py.list(ts),time,int32(x(2)));
+%
+%     end
+%
+% end
 
-    elseif x(2)==1
-       ts=py.mhkit_python_utils.pandas_dataframe.timeseries_to_pandas(py.list(ts),time,int32(x(2))); 
-       
-    end
-    
-end
-nnft=int32(nnft);
+ts = py.pandas.DataFrame(py.numpy.array(ts));
 
+nnft = int32(nnft);
 
-spectra=py.mhkit.wave.resource.elevation_spectrum(ts,sample_rate,nnft,pyargs('window',options.window,'detrend',options.detrend,'noverlap',options.noverlap));
- vals=double(py.array.array('d',py.numpy.nditer(spectra.values)));
- sha=cell(spectra.values.shape);
- x=int64(sha{1,1});
- y=int64(sha{1,2});
- vals=reshape(vals,[x,y]);
+spectra_py = py.mhkit.wave.resource.elevation_spectrum( ...
+    ts, sample_rate, nnft, ...
+    pyargs( ...
+        'window', options.window, ...
+        'detrend', options.detrend, ...
+        'noverlap', options.noverlap ...
+    ) ...
+);
 
+spectra_py = typecast_from_mhkit_python(spectra_py);
 
-S.spectrum=vals;
+S = struct();
 
-S.type='Spectra from Timeseries';
-
-S.frequency=double(py.array.array('d',py.numpy.nditer(spectra.index))).';
-S.sample_rate=sample_rate;
-S.nnft=nnft;
-
-    
+S.type = 'Spectra from Timeseries';
+S.spectrum = spectra_py.data;
+S.frequency = spectra_py.index.data;
+S.nnft = nnft;
+S.sample_rate = sample_rate;
