d69838e7c3ef14277694096351aa366037618353
Mohamed Shabara <84589678+MShabara@users.noreply.github.com>
2025-02-03 10:12:25 -0700

    Fixes Update docstrings, removes uncessary lines and txt file

--- mhkit/utils/upcrossing/uc_troughs.m
+++ mhkit/utils/upcrossing/uc_troughs.m
@@ -1,19 +1,27 @@
 function troughs = uc_troughs(t, data, inds)
-    % Finds the troughs between zero crossings.
-    %
-    % Parameters:
-    % t - Time array.
-    % data - Signal time-series.
-    % inds - Optional indices for the upcrossing.
-    %
-    % Returns:
-    % troughs - Trough values of the time-series
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Finds the troughs between zero crossings.
+%
+% Parameters
+% ----------
+% t : array
+%     Time array.
+% data : array
+%     Signal time-series.
+% inds : array, optional
+%     Indices for the upcrossing.
+%
+% Returns
+% -------
+% troughs : array
+%     Trough values of the time-series.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-    if nargin < 3
-        inds = upcrossing(t, data);
-    end
+if nargin < 3
+    inds = upcrossing(t, data);
+end
 
-    troughs = uc_apply_(t, data, @(ind1, ind2) min(data(ind1:ind2)), inds);
+troughs = uc_apply_(t, data, @(ind1, ind2) min(data(ind1:ind2)), inds);
 end
 
 
