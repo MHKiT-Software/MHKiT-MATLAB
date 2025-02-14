ae2ed2f09b2268fb30a2f3a50742d3fcb4d4946f
Simms, Andrew <Andrew.Simms@nrel.gov>
2025-02-14 08:05:16 -0700

    Examples: Improve whitespace and spelling in `loads_example`

--- examples/loads_example.m
+++ examples/loads_example.m
@@ -1,8 +1,11 @@
 %% Example: MHKiT-MATLAB Loads Example
+%
 % The following example will help familiarize you with some of the functions
 % in the <https://mhkit-software.github.io/MHKiT/mhkit-matlab/api.loads.html MHKiT
 % Loads Module> that you can use to assist you in your loads analysis.
+
 %% Import Loads Data
+%
 % In this example, since there is no known field loads data for MHK devices,
 % this data comes from a land-based wind turbine. We have a database of 331 files,
 % each containing 10 minutes of data sampled at 50Hz.
@@ -10,9 +13,12 @@
 % As a start, let's look at the data for one of these files to figure out what
 % formatting we need to apply.
 
-data=readtable('../examples/data/loads/data_loads_example.csv');
+data = readtable('./examples/data/loads/data_loads_example.csv');
 disp(data)
+
+%%
 % *Format Loads Data with datetime index*
+%
 % Our file above shows us that we have two references to time, but neither are
 % in the right format. The "Timestamp" column is what will give us the datetime
 % index that we are looking for, but we first need to convert it from Microsoft
@@ -20,10 +26,15 @@ disp(data)
 
 newtime = excel_to_datetime(data.Timestamp);
 data.time = newtime
+
 %% Loads Analysis
+%
 % Now that we have our loads data in the correct format for MHKiT, we do some
 % analysis.
-% Damage Equivalent Loads
+
+%%
+% *Damage Equivalent Loads*
+%
 % Let's say that we wanted to investigate fatigue. We can do this by calculating
 % short-term damage equivalent loads (DELs). To use this function, we need the
 % variable we want to analyze and its corresponding material slope factor. In
@@ -38,7 +49,10 @@ data.time = newtime
 % use the fatigue function to calculate the damage equivalent load for this file
 DEL_tower = damage_equivalent_load(data.TB_ForeAft,4,'bin_num',100,'data_length',600)
 DEL_blade = damage_equivalent_load(data.BL1_FlapMom,10,'bin_num',100,'data_length',600)
-% Calculate Loads Statistics
+
+%%
+% *Calculate Loads Statistics*
+%
 % Another important part of loads analysis is looking at statistics. Here, we
 % use another function to help us calculate the mean, max, min, and std for this
 % 10-minute file. Per standards, a valid statistical window has to be consecutive
@@ -50,13 +64,14 @@ DEL_blade = damage_equivalent_load(data.BL1_FlapMom,10,'bin_num',100,'data_lengt
 % windows. This function can still handle this scenario as long as the correct
 % inputs are specified.
 
-% First we need to convert the Matlab table to a structure
+% First we need to convert the table to a structure
 data = removevars(data,{'Timestamp','Time'});
 datast = table2struct(data,'ToScalar',true)
 % calculate the means, maxs, mins, and stdevs of this file
 stats = get_statistics(datast,50);
 % show the mean results
 disp(stats.mean)
+
 %%
 % At this point, it would be nice to start visualizing some of this data. In
 % order to do this, we need to calculate the stats and DELs for all the files
@@ -69,17 +84,21 @@ dmeans = table2struct(readtable('../examples/data/loads/data_loads_means.csv'),'
 dmaxs = table2struct(readtable('../examples/data/loads/data_loads_maxs.csv'),'ToScalar',true);
 dmins = table2struct(readtable('../examples/data/loads/data_loads_mins.csv'),'ToScalar',true);
 dstd = table2struct(readtable('../examples/data/loads/data_loads_std.csv'),'ToScalar',true);
-% *Plot Load Statistics*
+
+%%
+% *Visualizing Load Statistics*
+%
 % Now that we have all of our stats, let's make a scatter plot that can give
-% us a visual. Using the statplotter function, we can quickly create a standard
+% us a visual. Using the |plot_statistics| function, we can quickly create a standard
 % scatter plot showing how load variables trend with wind speed.  Using this we
 % can quickly spot expected trends and track down outliers.
 
 figure = plot_statistics(dmeans.uWind_80m,dmeans.BL1_FlapMom,dmins.BL1_FlapMom,dmaxs.BL1_FlapMom,"y_stdev",dstd.BL1_FlapMom,"xlabel",'Wind Speed [m/s]',"ylabel",'Blade Flap Mom [kNm]');
 figure2 = plot_statistics(dmeans.uWind_80m,dmeans.TB_ForeAft,dmins.TB_ForeAft,dmaxs.TB_ForeAft,"y_stdev",dstd.TB_ForeAft,"xlabel",'Wind Speed [m/s]',"ylabel",'Tower Base Mom [kNm]');
+
 %%
 % Another common step is to bin the statistical data. This can easily be done
-% with the bin_stats function from the loads module shown below. A warning message
+% with the |bin_statistics| function from the loads module shown below. A warning message
 % will show if there are any bins that were not filled.
 
 % create array containing wind speeds to use as bin edges
@@ -91,9 +110,10 @@ wind_min = bin_statistics(dmins,dmeans.uWind_80m,b_edges);
 wind_DEL = bin_statistics(dmaxs,dmeans.uWind_80m,b_edges);
 
 wind_min.averages
+
 %%
 % Now let's make some more plots with the binned data. Here we use the binned
-% data and corresponding standard deviations as inputs to the binplotter function.
+% data and corresponding standard deviations as inputs to the |plot_bin_statistics| function.
 
 bcenters = 3.5:1:24.5
-plot_bin_statistics(bcenters,wind_means.averages.TB_ForeAft,wind_max.averages.TB_ForeAft,wind_min.averages.TB_ForeAft,wind_means.std.TB_ForeAft,wind_max.std.TB_ForeAft,wind_min.std.TB_ForeAft,"xlabel",'Wind Speed [m/s]',"ylabel",'TB_ForeAft',"title",'Binned Stats');
+plot_bin_statistics(bcenters,wind_means.averages.TB_ForeAft,wind_max.averages.TB_ForeAft,wind_min.averages.TB_ForeAft,wind_means.std.TB_ForeAft,wind_max.std.TB_ForeAft,wind_min.std.TB_ForeAft,"xlabel",'Wind Speed [m/s]',"ylabel",'TB-ForeAft',"title",'Binned Stats');
