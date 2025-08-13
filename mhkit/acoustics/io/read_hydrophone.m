c4e2cde2fb3e79706ac86b11f13fd892fd0fe9dc
hivanov-nrel <Hristo.Ivanov@nrel.gov>
2025-08-13 16:15:53 -0600

    init module

---
+++ mhkit/acoustics/io/read_hydrophone.m
@@ -0,0 +1,16 @@
+function [outputArg1,outputArg2] = read_hydrophone(inputArg1,inputArg2)
+%READ_HYDROPHONE Summary of this function goes here
+%   Detailed explanation goes here
+arguments (Input)
+    inputArg1
+    inputArg2
+end
+
+arguments (Output)
+    outputArg1
+    outputArg2
+end
+
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
