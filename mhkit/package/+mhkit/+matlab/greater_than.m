458a011353675dff8a59559faf781ba045887743
Simms, Andrew <Andrew.Simms@nrel.gov>
2025-09-07 18:00:52 -0600

    Install: Add docstrings to matlab commands

--- mhkit/package/+mhkit/+matlab/greater_than.m
+++ mhkit/package/+mhkit/+matlab/greater_than.m
@@ -1,4 +1,24 @@
 function result = greater_than(maximum_version)
-    disp(maximum_version);
-    result = isMATLABReleaseOlderThan(maximum_version);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   Check if current MATLAB version is greater than specified version
+%
+% Parameters
+% ------------
+%     maximum_version: string
+%         Maximum tested MATLAB version (e.g., 'R2024b')
+%
+% Returns
+% ---------
+%     result: logical
+%         true if current MATLAB version is greater than maximum_version
+%         false otherwise
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    % Check if current MATLAB version is newer than the specified version
+    % isMATLABReleaseOlderThan returns true if current < specified
+    % We want true if current > specified
+    current_release = version('-release');
+    result = ~isMATLABReleaseOlderThan(maximum_version) && ~strcmp(current_release, maximum_version);
 end
--- mhkit/package/+mhkit/+matlab/less_than.m
+++ mhkit/package/+mhkit/+matlab/less_than.m
@@ -1,3 +1,20 @@
 function result = less_than(minimum_version)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   Check if current MATLAB version is less than specified version
+%
+% Parameters
+% ------------
+%     minimum_version: string
+%         Minimum required MATLAB version (e.g., 'R2022b')
+%
+% Returns
+% ---------
+%     result: logical
+%         true if current MATLAB version is less than minimum_version
+%         false otherwise
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
     result = isMATLABReleaseOlderThan(minimum_version);
 end
