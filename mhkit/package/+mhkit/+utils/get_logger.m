b3774d51944143384d1dafd8f4dd19e2f4e2cce8
Simms, Andrew <Andrew.Simms@nrel.gov>
2025-09-07 18:02:11 -0600

    Install: Add docstrings to utils commands

--- mhkit/package/+mhkit/+utils/get_logger.m
+++ mhkit/package/+mhkit/+utils/get_logger.m
@@ -1,8 +1,23 @@
 function logger = get_logger()
-    % GET_LOGGER Returns a simple logger struct with basic logging functions
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   Returns a simple logger struct with basic logging functions
+%
+% Parameters
+% ------------
+%     None
+%
+% Returns
+% ---------
+%     logger: struct
+%         Logger structure with debug, info, warning, and error functions
+%         Each function accepts a format string and optional arguments
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
     logger = struct();
     logger.debug = @(msg, varargin) fprintf(['MHKiT Debug: ' msg '\n'], varargin{:});
-    logger.info = @(msg, varargin) fprintf('MHKiT: %s\n', sprintf(msg, varargin{:}));
+    logger.info = @(msg, varargin) fprintf('%s\n', sprintf(msg, varargin{:}));
     logger.warning = @(msg, varargin) fprintf('MHKiT Warning: %s\n', sprintf(msg, varargin{:}));
     logger.error = @(msg, varargin) fprintf('MHKiT Error: %s\n', sprintf(msg, varargin{:}));
 end
