216b6fa246a652b62b1811c0025171ba1372bcdb
Simms, Andrew <Andrew.Simms@nrel.gov>
2025-09-09 08:22:28 -0600

    Signal: Add rectwin window

---
+++ mhkit/signal_processing/mhkit_window_rectwin.m
@@ -0,0 +1,29 @@
+function win = mhkit_window_rectwin(n_fft)
+
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%     Generate rectangular window for signal processing applications
+%
+% Parameters
+% ------------
+%   n_fft: double
+%       Length of the window (number of samples)
+%
+% Returns
+% ---------
+%   win: double
+%       Rectangular window values as column vector [n_fft x 1]
+%       All values are 1
+%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+arguments
+    n_fft (1,1) {mustBeNumeric, mustBePositive, mustBeInteger}
+end
+
+n_fft = double(n_fft);
+
+% Create rectangular window (all ones)
+win = ones(n_fft, 1);
+
+end
