786bfdb9c56783e96980b5cac785208e702e5e37
Simms, Andrew <Andrew.Simms@nrel.gov>
2025-09-29 12:40:39 -0600

    Install: Remove non working path check from check_health()

--- mhkit/package/+mhkit/check_health.m
+++ mhkit/package/+mhkit/check_health.m
@@ -116,13 +116,6 @@ function result = check_health()
             end
             logger.info('Platform: %s', platform);
             
-            % Check PATH for Python
-            current_path = getenv('PATH');
-            if contains(current_path, 'python') || contains(current_path, 'conda')
-                logger.info('✓ Python/Conda found in system PATH');
-            else
-                logger.warning('⚠ Python/Conda not detected in system PATH');
-            end
             
         catch ME
             logger.warning('⚠ Could not gather all system information: %s', ME.message);
